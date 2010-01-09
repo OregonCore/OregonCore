0ec690c9a434792c043ef339e97ccf84d23713c0
MiLk <emilien_kenler@hotmail.com>
2010-01-09 22:21:13 +0100

    Change TC> into Oregon> in console

--- src/oregoncore/CliRunnable.cpp
+++ src/oregoncore/CliRunnable.cpp
@@ -377,7 +377,7 @@ void CliRunnable::run()
 
     // print this here the first time
     // later it will be printed after command queue updates
-    printf("TC>");
+    printf("Oregon>");
 
     ///- As long as the World is running (no World::m_stopEvent), get the command line and handle it
     while (!World::IsStopped())
@@ -389,7 +389,7 @@ void CliRunnable::run()
 	#if PLATFORM == WINDOWS
 	command_str = fgets(commandbuf,sizeof(commandbuf),stdin);
 	#else
-	command_str = readline("TC>");
+	command_str = readline("Oregon>");
 	rl_bind_key('\t',rl_complete);
 	#endif
 	if (command_str != NULL)
@@ -405,7 +405,7 @@ void CliRunnable::run()
             if(!*command_str)
             {
 	      #if PLATFORM == WINDOWS
-	        printf("TC>");
+	        printf("Oregon>");
 	      #endif
                 continue;
             }
@@ -414,7 +414,7 @@ void CliRunnable::run()
             if(!consoleToUtf8(command_str,command))         // convert from console encoding to utf8
             {
 	      #if PLATFORM == WINDOWS
-	        printf("TC>");
+	        printf("Oregon>");
 	      #endif
                 continue;
             }
