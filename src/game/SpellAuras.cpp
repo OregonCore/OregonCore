51b2094948764922d135fd26136effb4e58c4a8e
MiLk <emilien_kenler@hotmail.com>
2010-01-10 10:42:57 +0100

    Implement spell 7057. from MaNGOS

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -1986,6 +1986,12 @@ void Aura::HandleAuraDummy(bool apply, bool Real)
                 if( caster && m_target->CanHaveThreatList())
                     m_target->AddThreat(caster, 10.0f);
                 return;
+            case 7057:                                      // Haunting Spirits
+                // expected to tick with 30 sec period (tick part see in Aura::PeriodicTick)
+                m_isPeriodic = true;
+                m_modifier.periodictime = 30*IN_MILISECONDS;
+                m_periodicTimer = m_modifier.periodictime;
+                return;
             case 13139:                                     // net-o-matic
                 // root to self part of (root_target->charge->root_self sequence
                 if(caster)
@@ -6129,6 +6135,7 @@ void Aura::PeriodicTick()
             break;
         }
         // Here tick dummy auras
+        case SPELL_AURA_DUMMY:                              // some spells have dummy aura
         case SPELL_AURA_PERIODIC_DUMMY:
         {
             PeriodicDummyTick();
@@ -6206,6 +6213,12 @@ void Aura::PeriodicDummyTick()
                     return;
                 }
             }
+            return;
+        }
+        case 7057:                                  // Haunting Spirits
+        {
+            if (roll_chance_i(33))
+                m_target->CastSpell(m_target,m_modifier.m_amount,true,NULL,this);
             return;
         }
 //        // Panda
