826fa3d69942977f2dbf45f32c0a229518c2b4c2
avoid <none@none>
2010-01-10 20:31:29 +0100

    Added SQL-File for Hexlord (ZA)

---
+++ sql/updates/26_world.sql
@@ -0,0 +1,25 @@
+/*
+MySQL Data Transfer
+Source Host: 192.168.32.218
+Source Database: world
+Target Host: 192.168.32.218
+Target Database: world
+Date: 08.01.2010 16:44:46
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+-- ----------------------------
+-- Table structure for spell_disabled
+-- ----------------------------
+DROP TABLE IF EXISTS `spell_disabled`;
+CREATE TABLE `spell_disabled` (
+  `entry` int(11) unsigned NOT NULL default '0' COMMENT 'Spell entry',
+  `disable_mask` int(8) unsigned NOT NULL default '0',
+  `comment` varchar(64) NOT NULL default '',
+  PRIMARY KEY  (`entry`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='Disabled Spell System';
+
+-- ----------------------------
+-- Records 
+-- ----------------------------
+INSERT INTO `spell_disabled` VALUES ('44132', '1', '\'1% Schandeserhöhung\' Disable spell for players');
