485a832e970e7da90eecd9970590048c55296c49
Dave <davevmb@gmail.com>
2010-01-20 23:06:09 +0100

    and some more adjusted copyright lines, almost all done :D

--- src/game/InstanceSaveMgr.cpp
+++ src/game/InstanceSaveMgr.cpp
@@ -1,7 +1,7 @@
 /*
  * Copyright (C) 2005-2008 MaNGOS <http://www.mangosproject.org/>
  *
- * Copyright (C) 2008 Oregon <http://www.oregoncore.com/>
+ * Copyright (C) 2008 Trinity <http://www.trinitycore.org/>
  *
  *
  * This program is free software; you can redistribute it and/or modify
--- src/game/TicketHandler.cpp
+++ src/game/TicketHandler.cpp
@@ -1,7 +1,7 @@
 /*
  * Copyright (C) 2005-2008 MaNGOS
  *
- * Copyright (C) 2008 Oregon
+ * Copyright (C) 2008 Trinity
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
