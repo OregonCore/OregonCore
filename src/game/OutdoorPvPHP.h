4482e408153a96fb998753ee2c84d7a600983d58
Dave <davevmb@gmail.com>
2010-01-20 20:26:57 +0100

    third wave of changes to the copyright lines ->Trinity

--- src/game/OutdoorPvPHP.h
+++ src/game/OutdoorPvPHP.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2008 Oregon <http://www.oregoncore.com/>
+ * Copyright (C) 2008 Trinity <http://www.trinitycore.org/>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
