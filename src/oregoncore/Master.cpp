63436d292951fceebdbd7bbce061d2c13e6d6a0e
Dave <davevmb@gmail.com>
2010-01-24 13:51:48 +0100

    fixed compile and removed some  warnings while compiling

--- src/oregoncore/Master.cpp
+++ src/oregoncore/Master.cpp
@@ -199,16 +199,17 @@ int Master::Run()
 {
     sLog.outString( "%s (core-daemon)", _FULLVERSION );
     sLog.outString( "<Ctrl-C> to stop.\n" );
-
+// Remove the warnings C4129 while compiling
+#pragma warning (disable : 4129)
     sLog.outTitle( " _____                                      ");
     sLog.outTitle( "/\  __`\                                    ");
     sLog.outTitle( "\ \ \/\ \  _ __   __     __     ___    ___  ");
     sLog.outTitle( " \ \ \ \ \/\`'__\'__`\ /'_ `\  / __`\/' _ `\ ");
     sLog.outTitle( "  \ \ \_\ \ \ \/\  __//\ \L\ \/\ \L\ \\ \/\ \ ");
-    sLog.outTitle( "   \ \_____\ \_\ \____\ \____ \ \____/ \_\ \_\");
-    sLog.outTitle( "    \/_____/\/_/\/____/\/___L\ \/___/ \/_/\/_/");
-    sLog.outTitle( "                         /\____/              ");
-	sLog.outTitle( "                         \_/__/               ");
+    sLog.outTitle( "   \ \_____\ \_\ \____\ \____ \ \____/ \_\ \_\ ");
+    sLog.outTitle( "    \/_____/\/_/\/____/\/___L\ \/___/ \/_/\/_/ ");
+    sLog.outTitle( "                         /\____/               ");
+	sLog.outTitle( "                         \_/__/                ");
     sLog.outTitle( "http://www.OregonCore.com                    \n");
 
     /// worldd PID file creation
