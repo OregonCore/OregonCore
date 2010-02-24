8b99770dd716594122881b71db8d77ff231cb2c4
Dave <davevmb@gmail.com>
2010-02-24 10:48:05 +0100

    Fixing windows build again, commiting the netherspite script (thx to lovelol), removing zthread files that are no longer needed.

--- src/shared/revision.h
+++ src/shared/revision.h
@@ -1,11 +1,11 @@
 #ifndef __REVISION_H__
 #define __REVISION_H__
- #define _REVISION "81"
- #define _HASH "273577be49ba"
+ #define _REVISION "82"
+ #define _HASH "b50cc9b92b89"
  #define _REVISION_DATE "*"
  #define _REVISION_TIME "*"
- #define FILEVER        0,0,81,0
- #define PRODUCTVER     0,0,81,0
- #define STRFILEVER     "0, 0, 81, 273577be49ba"
- #define STRPRODUCTVER  "0, 0, 81, 273577be49ba"
+ #define FILEVER        0,0,82,0
+ #define PRODUCTVER     0,0,82,0
+ #define STRFILEVER     "0, 0, 82, b50cc9b92b89"
+ #define STRPRODUCTVER  "0, 0, 82, b50cc9b92b89"
 #endif // __REVISION_H__
