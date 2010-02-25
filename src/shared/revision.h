47fce81cb614a68a2cfdab8ba8abf4096b66e5d4
Dave <davevmb@gmail.com>
2010-02-25 16:58:32 +0100

    adding some correct values for x64 building on windows, however still not building...

--- src/shared/revision.h
+++ src/shared/revision.h
@@ -1,11 +1,11 @@
 #ifndef __REVISION_H__
 #define __REVISION_H__
- #define _REVISION "82"
- #define _HASH "b50cc9b92b89"
+ #define _REVISION "84"
+ #define _HASH "3ea9804ea89c"
  #define _REVISION_DATE "*"
  #define _REVISION_TIME "*"
- #define FILEVER        0,0,82,0
- #define PRODUCTVER     0,0,82,0
- #define STRFILEVER     "0, 0, 82, b50cc9b92b89"
- #define STRPRODUCTVER  "0, 0, 82, b50cc9b92b89"
+ #define FILEVER        0,0,84,0
+ #define PRODUCTVER     0,0,84,0
+ #define STRFILEVER     "0, 0, 84, 3ea9804ea89c"
+ #define STRPRODUCTVER  "0, 0, 84, 3ea9804ea89c"
 #endif // __REVISION_H__
