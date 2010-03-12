154378a7f745da7a2cf3249041ede61701489416
Dave <davevmb@gmail.com>
2010-03-12 21:17:42 +0100

    Some preparations for the scriptsystem in VS game project. Starting this revision i will be modifying the core slowly adding in the modified script system. It may occur that the newest build doesnt build. I will try to avoid this as much as possible and fix it if it does occur. Once the process is completed i will mark that release as stable ;)

--- src/shared/revision.h
+++ src/shared/revision.h
@@ -1,11 +1,11 @@
 #ifndef __REVISION_H__
 #define __REVISION_H__
- #define _REVISION "114"
- #define _HASH "2590aeccefda"
+ #define _REVISION "126"
+ #define _HASH "b138fc75fe32"
  #define _REVISION_DATE "*"
  #define _REVISION_TIME "*"
- #define FILEVER        0,0,114,0
- #define PRODUCTVER     0,0,114,0
- #define STRFILEVER     "0, 0, 114, 2590aeccefda"
- #define STRPRODUCTVER  "0, 0, 114, 2590aeccefda"
+ #define FILEVER        0,0,126,0
+ #define PRODUCTVER     0,0,126,0
+ #define STRFILEVER     "0, 0, 126, b138fc75fe32"
+ #define STRPRODUCTVER  "0, 0, 126, b138fc75fe32"
 #endif // __REVISION_H__
