c8a64087c4c8bec46b5685482433105af1e5e2f6
Dave <davevmb@gmail.com>
2010-03-31 20:46:14 +0200

    Fix shield block value pct mod use (apply it on strenght bonus also) Author DiSlord

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -4450,7 +4450,7 @@ uint32 Player::GetShieldBlockValue() const
 {
     BaseModGroup modGroup = SHIELD_BLOCK_VALUE;
 
-    float value = GetTotalBaseModValue(modGroup) + GetStat(STAT_STRENGTH)/20 - 1;
+    float value = (m_auraBaseMod[SHIELD_BLOCK_VALUE][FLAT_MOD] + GetStat(STAT_STRENGTH) * 0.5f - 10)*m_auraBaseMod[SHIELD_BLOCK_VALUE][PCT_MOD];
 
     value = (value < 0) ? 0 : value;
 
