f83739b6e9492926a8d474cf636c70f18d231c33
Dave <davevmb@gmail.com>
2010-04-05 12:55:18 +0200

    Fix abug that pet does not toggle autocast when loaded

--- src/game/Pet.cpp
+++ src/game/Pet.cpp
@@ -307,8 +307,14 @@ bool Pet::LoadPetFromDB( Unit* owner, uint32 petentry, uint32 petnumber, bool cu
 
             // patch for old data where some spells have ACT_DECIDE but should have ACT_CAST
             // so overwrite old state
-            SpellEntry const *spellInfo = sSpellStore.LookupEntry(m_charmInfo->GetActionBarEntry(index)->SpellOrAction);
-            if (spellInfo && spellInfo->AttributesEx & SPELL_ATTR_EX_UNAUTOCASTABLE_BY_PET) m_charmInfo->GetActionBarEntry(index)->Type = ACT_CAST;
+           if(SpellEntry const *spellInfo = sSpellStore.LookupEntry(m_charmInfo->GetActionBarEntry(index)->SpellOrAction))
+		   {
+			   if (spellInfo && spellInfo->AttributesEx & SPELL_ATTR_EX_UNAUTOCASTABLE_BY_PET)
+				   m_charmInfo->GetActionBarEntry(index)->Type == ACT_DISABLED;
+
+			   if(m_charmInfo->GetActionBarEntry(index)->Type == ACT_ENABLED)
+				   ToggleAutocast(spellInfo->Id, true);
+		   }
         }
 
         //init teach spells
--- src/shared/revision.h
+++ src/shared/revision.h
@@ -1,11 +1,11 @@
 #ifndef __REVISION_H__
 #define __REVISION_H__
- #define _REVISION "146"
- #define _HASH "b6c4da65d16b"
+ #define _REVISION "148"
+ #define _HASH "90a23dad187b"
  #define _REVISION_DATE "*"
  #define _REVISION_TIME "*"
- #define FILEVER        0,0,146,0
- #define PRODUCTVER     0,0,146,0
- #define STRFILEVER     "0, 0, 146, b6c4da65d16b"
- #define STRPRODUCTVER  "0, 0, 146, b6c4da65d16b"
+ #define FILEVER        0,0,148,0
+ #define PRODUCTVER     0,0,148,0
+ #define STRFILEVER     "0, 0, 148, 90a23dad187b"
+ #define STRPRODUCTVER  "0, 0, 148, 90a23dad187b"
 #endif // __REVISION_H__
