851b82197680f94ff011c6ae5f212b36965f18b1
stfx <stfx@hotmail.de>
2010-06-24 10:26:05 +0200

    TC 3836: Automatically revive players when they fall under the map TC 4036: Teleport player to home if player falls under the map and no close grave is available

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -4277,7 +4277,7 @@ void Player::RepopAtGraveyard()
     AreaTableEntry const *zone = GetAreaEntryByAreaID(GetAreaId());
 
     // Such zones are considered unreachable as a ghost and the player must be automatically revived
-    if (!isAlive() && zone && zone->flags & AREA_FLAG_NEED_FLY || GetTransport())
+    if ((!isAlive() && zone && zone->flags & AREA_FLAG_NEED_FLY) || GetTransport() || GetPositionZ() < -500.0f)
     {
         ResurrectPlayer(0.5f);
         SpawnCorpseBones();
@@ -4311,6 +4311,8 @@ void Player::RepopAtGraveyard()
             GetSession()->SendPacket(&data);
         }
     }
+    else if (GetPositionZ() < -500.0f)
+        TeleportTo(m_homebindMapId, m_homebindX, m_homebindY, m_homebindZ, GetOrientation());
 }
 
 void Player::JoinedChannel(Channel *c)
