7c803eba4b6bc5c56dfc2ee200718fa13174761b
desteny <evo_racer@web.de>
2010-09-10 02:09:15 +0200

    Missing Line in the last Commit.

--- sql/updates/702_world_structure.sql
+++ sql/updates/702_world_structure.sql
@@ -2,6 +2,7 @@ ALTER TABLE `event_scripts` ADD PRIMARY KEY  (`id`, `delay`, `command`, `datalon
 ALTER TABLE `gameobject_scripts` ADD PRIMARY KEY  (`id`, `delay`, `command`, `datalong`, `datalong2`, `dataint`, `x`, `y`, `z`, `o`);
 ALTER TABLE `gossip_scripts` ADD PRIMARY KEY  (`id`, `delay`, `command`, `datalong`, `datalong2`, `dataint`, `x`, `y`, `z`, `o`);
 ALTER TABLE `spell_scripts` ADD PRIMARY KEY  (`id`, `delay`, `command`, `datalong`, `datalong2`, `dataint`, `x`, `y`, `z`, `o`);
+ALTER TABLE `quest_start_scripts` ADD PRIMARY KEY  (`id`, `delay`, `command`, `datalong`, `datalong2`, `dataint`, `x`, `y`, `z`, `o`);
 
 DELETE FROM `quest_end_scripts` WHERE `id` = 10964 AND `delay` = 38 AND `command` = 2 AND `datalong` = 159 AND `datalong2` = 3 AND `dataint` = 0 AND  `x` = 0 AND  `y` = 0 AND  `z` = 0 AND `o` = 0;
 INSERT INTO `quest_end_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `dataint`, `x`, `y`, `z`, `o`) VALUES('10964','38','2','159','3','0','0','0','0','0');
