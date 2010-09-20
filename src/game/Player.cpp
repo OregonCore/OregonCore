16c2e3cc20bd82de62b488308c7b70d2c7545f06
desteny <evo_racer@web.de>
2010-09-21 01:56:31 +0200

    Typo Fix for Rev 723 thx Celtus

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -6056,7 +6056,7 @@ void Player::RewardReputation(Quest const *pQuest)
     {
         if (pQuest->RewRepFaction[i] && pQuest->RewRepValue[i])
         {
-            int32 rep = CalculateReputationGain(GetQuestOrPlayerLevel(pQuest), pQuest->RewRepValue[i], true);
+            int32 rep = CalculateReputationGain(GetQuestLevelForPlayer(pQuest), pQuest->RewRepValue[i], true);
             FactionEntry const* factionEntry = sFactionStore.LookupEntry(pQuest->RewRepFaction[i]);
             if (factionEntry)
                 ModifyFactionReputation(factionEntry, rep);
