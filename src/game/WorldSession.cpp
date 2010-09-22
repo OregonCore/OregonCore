2c1ec4df70ed8b47b2fb821a7702f5d845532cc0
stfx <stfx@hotmail.de>
2010-09-22 20:38:44 +0200

    [mangos-0.12] Fix teleporting players home at logging out in invalid instances

--- src/game/WorldSession.cpp
+++ src/game/WorldSession.cpp
@@ -341,6 +341,12 @@ void WorldSession::LogoutPlayer(bool Save)
         if (BattleGround *bg = _player->GetBattleGround())
             bg->EventPlayerLoggedOut(_player);
 
+        ///- Teleport to home if the player is in an invalid instance
+        if (!_player->m_InstanceValid && !_player->isGameMaster())
+        {
+            _player->TeleportTo(_player->m_homebindMapId, _player->m_homebindX, _player->m_homebindY, _player->m_homebindZ, _player->GetOrientation());
+        }
+
         sOutdoorPvPMgr.HandlePlayerLeaveZone(_player,_player->GetZoneId());
 
         for (int i=0; i < PLAYER_MAX_BATTLEGROUND_QUEUES; ++i)
