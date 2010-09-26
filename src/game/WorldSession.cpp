3014210c1ed849a4c19286f761bd424b88032972
stfx <stfx@hotmail.de>
2010-09-26 18:42:04 +0200

    [mangos-0.12] Fix problem with Alt+F4 in instances

--- src/game/WorldSession.cpp
+++ src/game/WorldSession.cpp
@@ -339,9 +339,12 @@ void WorldSession::LogoutPlayer(bool Save)
             _player->TeleportToHomebind();
             //this is a bad place to call for far teleport because we need player to be in world for successful logout
             //maybe we should implement delayed far teleport logout?
-            while (_player->IsBeingTeleportedFar())
-                HandleMoveWorldportAckOpcode();
         }
+
+        // FG: finish pending transfers after starting the logout
+        // this should fix players beeing able to logout and login back with full hp at death position
+        while (_player->IsBeingTeleportedFar())
+            HandleMoveWorldportAckOpcode();
 
         sOutdoorPvPMgr.HandlePlayerLeaveZone(_player,_player->GetZoneId());
 
