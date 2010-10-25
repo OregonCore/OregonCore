870db752b54e9b5f176ee8ab7e5e923c20b100ff
stfx <stfx@hotmail.de>
2010-10-25 18:29:20 +0200

    Try to fix linux build of vmap extractor

--- src/tools/vmap_extractor/loadlib/loadlib.h
+++ src/tools/vmap_extractor/loadlib/loadlib.h
@@ -1,7 +1,7 @@
 #ifndef LOAD_LIB_H
 #define LOAD_LIB_H
 
-#ifdef WIN32
+#ifdef _WIN32
 typedef __int64            int64;
 typedef __int32            int32;
 typedef __int16            int16;
@@ -12,9 +12,9 @@ typedef unsigned __int16   uint16;
 typedef unsigned __int8    uint8;
 #else
 #include <stdint.h>
-#ifndef uint64_t 
+#ifndef uint64_t
 #ifdef __linux__
-#include <linux/types.h> 
+#include <linux/types.h>
 #endif
 #endif
 typedef int64_t            int64;
