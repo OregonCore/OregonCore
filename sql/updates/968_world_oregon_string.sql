2840e541d95273c1163eac33d1a07a2f587c7ec8
kerhong <none@none>
2010-11-02 20:23:24 +0200

    Fix .mute

--- sql/updates/968_world_oregon_string.sql
+++
@@ -1,1 +0,0 @@
-UPDATE oregon_string SET content_default = 'Your chat has been disabled by %s for %u minutes. Reason: %s.' WHERE oregon_string.entry=300;
---
+++ sql/updates/969_world_oregon_string.sql
@@ -0,0 +1,1 @@
+UPDATE oregon_string SET content_default = 'Your chat has been disabled for %u minutes. Reason: %s.' WHERE oregon_string.entry=300;
