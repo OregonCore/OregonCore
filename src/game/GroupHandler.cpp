2494ce278d3d2a32cb4bee5f2d30ebe3b6f1c9d1
lukaasm <none@none>
2011-01-03 12:39:53 +0100

    Fix players being displayed as offline in group view after being teleported to a different map

--- src/game/GroupHandler.cpp
+++ src/game/GroupHandler.cpp
@@ -795,7 +795,7 @@ void WorldSession::HandleRequestPartyMemberStatsOpcode(WorldPacket &recv_data)
     uint64 Guid;
     recv_data >> Guid;
 
-    Player *player = objmgr.GetPlayer(Guid);
+    Player *player = HashMapHolder<Player>::Find(Guid);
     if (!player)
     {
         WorldPacket data(SMSG_PARTY_MEMBER_STATS_FULL, 3+4+2);
