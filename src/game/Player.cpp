d34ca543e724de92b13985460c8031feba27bd16
stfx <stfx@hotmail.de>
2011-01-04 00:28:03 +0100

    TC 9625: Fix arena visibility (no more seeing opponents before arena starts)

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -18560,6 +18560,11 @@ bool Player::canSeeOrDetect(Unit const* u, bool detect, bool inVisibleList, bool
     if (u == this)
         return true;
 
+    // Arena visibility before arena start
+    if (InArena() && GetBattleGround() && GetBattleGround()->GetStatus() == STATUS_WAIT_JOIN)
+        if (const Player* target = u->GetCharmerOrOwnerPlayerOrPlayerItself())
+            return GetBGTeam() == target->GetBGTeam();
+
     // player visible for other player if not logout and at same transport
     // including case when player is out of world
     bool at_same_transport =
