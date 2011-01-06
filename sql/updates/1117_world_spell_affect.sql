87b71c6fd835fe2130b0ce0987c3ef0fb8bdf83f
stfx <stfx@hotmail.de>
2011-01-06 18:42:29 +0100

    And another typofix of commit 1117. Removes duplicate entries :)

--- sql/updates/1117_world_spell_affect.sql
+++ sql/updates/1117_world_spell_affect.sql
@@ -1,4 +1,4 @@
-delete from `spell_affect` where `entry` = '36563';
-insert into `spell_affect` (`entry`, `effectId`, `SpellFamilyMask`) values
+DELETE FROM `spell_affect` WHERE `entry`=36563;
+INSERT INTO `spell_affect` (`entry`, `effectId`, `SpellFamilyMask`) VALUES
 (36563,1,0x1062612030F),
 (36563,2,0x1062612030F);
