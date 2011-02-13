037f3c927c5844a74190d664e02016eeaa6527c7
henhouse <henry925@gmail.com>
2011-02-13 03:13:41 -0500

    Missing part of 1288. Why am I so bad to remember SQL?

--- sql/scripts/world_scripts_full.sql
+++ sql/scripts/world_scripts_full.sql
@@ -723,6 +723,7 @@ UPDATE `creature_template` SET `ScriptName`='boss_onyxia' WHERE `entry`=10184;
 UPDATE `creature_template` SET `ScriptName`='npc_neeru_fireblade' WHERE `entry`=3216;
 UPDATE `creature_template` SET `ScriptName`='npc_shenthul' WHERE `entry`=3401;
 UPDATE `creature_template` SET `ScriptName`='npc_thrall_warchief' WHERE `entry`=4949;
+UPDATE `creature_template` SET `ScriptName`='npc_eitrigg' WHERE `entry`=3144;
 
 /* RAGEFIRE CHASM */
 UPDATE `gameobject_template` SET `ScriptName`='go_blood_filled_orb' WHERE `entry`=182024;
