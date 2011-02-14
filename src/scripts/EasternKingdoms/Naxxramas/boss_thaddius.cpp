937cb2939fb56af7fa6d9a16513ad9ff4fc3a371
stfx <stfx@hotmail.de>
2011-02-15 00:29:42 +0100

    Fix warnings in thaddius script and reordered them with thaddius being first

--- src/scripts/EasternKingdoms/Naxxramas/boss_thaddius.cpp
+++ src/scripts/EasternKingdoms/Naxxramas/boss_thaddius.cpp
@@ -99,24 +99,19 @@ enum eEvents
     EVENT_BERSERK        = 7
 };
 
-struct boss_fuegenAI : public BossAI
+struct boss_thaddiusAI : public BossAI
 {
-    boss_fuegenAI(Creature *c): BossAI(c, EVENT_FUEGEN){}
+    boss_thaddiusAI(Creature *c) : BossAI(c, EVENT_THADDIUS)
 
     void Reset()
     {
         //ClearCastQueue();
 
-        // proper timers
-        events.ScheduleEvent(EVENT_CHECK_COIL, 2000);
-        events.ScheduleEvent(EVENT_PULL_TANK, 20500);
-
-        // guessed timers, to FIX
-        events.ScheduleEvent(EVENT_MANA_BURN, 10000);
-        events.ScheduleEvent(EVENT_WAR_STOMP, 30000);
+        events.ScheduleEvent(EVENT_POLARITY_SHIFT, 30000);
+        events.ScheduleEvent(EVENT_BERSERK, 300000);
     }
 
-    void UpdateAI(uint32 diff)
+    void UpdateAI(const uint32 diff)
     {
         if (!UpdateVictim())
             return;
@@ -126,29 +121,15 @@ struct boss_fuegenAI : public BossAI
         {
             switch(eventId)
             {
-                case EVENT_CHECK_COIL:
+                case EVENT_POLARITY_SHIFT:
                 {
-                    events.ScheduleEvent(EVENT_CHECK_COIL, 2000);
+                    //AddAOESpellToCast(SPELL_POLARITY_SHIFT);
+                    events.ScheduleEvent(EVENT_POLARITY_SHIFT, 30000);
                     break;
                 }
-                case EVENT_MANA_BURN:
+                case EVENT_BERSERK:
                 {
-                    //AddAOESpellToCast(SPELL_MANA_BURN);
-                    events.ScheduleEvent(EVENT_MANA_BURN, 10000); // guessed timer
-                    break;
-                }
-                case EVENT_WAR_STOMP:
-                {
-                    //AddAOESpellToCast(SPELL_WAR_STOMP);
-                    events.ScheduleEvent(EVENT_WAR_STOMP, 15000); // guessed timer
-                    break;
-                }
-                case EVENT_PULL_TANK:
-                {
-                    Unit *pStalagg = NULL;
-                    // Select Stalaggs as target, rest will be handled in scripteffect
-                    //AddSpellToCast(pStalagg, SPELL_MAGNETIC_PULL);
-                    events.ScheduleEvent(EVENT_PULL_TANK, 20500);
+                    //AddSpellToCast(me, SPELL_BERSERK);
                     break;
                 }
                 default: break;
@@ -162,7 +143,7 @@ struct boss_fuegenAI : public BossAI
 
 struct boss_stalaggAI : public BossAI
 {
-    boss_stalaggAI(Creature *c) : BossAI(c, EVENT_STALAGG) {}
+    boss_stalaggAI(Creature *c) : BossAI(c, EVENT_STALAGG)
 
     void Reset()
     {
@@ -177,7 +158,7 @@ struct boss_stalaggAI : public BossAI
         events.ScheduleEvent(EVENT_WAR_STOMP, 30000);
     }
 
-    void UpdateAI(uint32 diff)
+    void UpdateAI(const uint32 diff)
     {
         if (!UpdateVictim())
             return;
@@ -221,19 +202,24 @@ struct boss_stalaggAI : public BossAI
     }
 };
 
-struct boss_thaddiusAI : public BossAI
+struct boss_fuegenAI : public BossAI
 {
-    boss_thaddiusAI(Creature *c) : BossAI(c, EVENT_THADDIUS) {}
+    boss_fuegenAI(Creature *c): BossAI(c, EVENT_FUEGEN)
 
     void Reset()
     {
         //ClearCastQueue();
 
-        events.ScheduleEvent(EVENT_POLARITY_SHIFT, 30000);
-        events.ScheduleEvent(EVENT_BERSERK, 300000);
+        // proper timers
+        events.ScheduleEvent(EVENT_CHECK_COIL, 2000);
+        events.ScheduleEvent(EVENT_PULL_TANK, 20500);
+
+        // guessed timers, to FIX
+        events.ScheduleEvent(EVENT_MANA_BURN, 10000);
+        events.ScheduleEvent(EVENT_WAR_STOMP, 30000);
     }
 
-    void UpdateAI(uint32 diff)
+    void UpdateAI(const uint32 diff)
     {
         if (!UpdateVictim())
             return;
@@ -243,15 +229,29 @@ struct boss_thaddiusAI : public BossAI
         {
             switch(eventId)
             {
-                case EVENT_POLARITY_SHIFT:
+                case EVENT_CHECK_COIL:
                 {
-                    //AddAOESpellToCast(SPELL_POLARITY_SHIFT);
-                    events.ScheduleEvent(EVENT_POLARITY_SHIFT, 30000);
+                    events.ScheduleEvent(EVENT_CHECK_COIL, 2000);
                     break;
                 }
-                case EVENT_BERSERK:
+                case EVENT_MANA_BURN:
                 {
-                    //AddSpellToCast(me, SPELL_BERSERK);
+                    //AddAOESpellToCast(SPELL_MANA_BURN);
+                    events.ScheduleEvent(EVENT_MANA_BURN, 10000); // guessed timer
+                    break;
+                }
+                case EVENT_WAR_STOMP:
+                {
+                    //AddAOESpellToCast(SPELL_WAR_STOMP);
+                    events.ScheduleEvent(EVENT_WAR_STOMP, 15000); // guessed timer
+                    break;
+                }
+                case EVENT_PULL_TANK:
+                {
+                    Unit *pStalagg = NULL;
+                    // Select Stalaggs as target, rest will be handled in scripteffect
+                    //AddSpellToCast(pStalagg, SPELL_MAGNETIC_PULL);
+                    events.ScheduleEvent(EVENT_PULL_TANK, 20500);
                     break;
                 }
                 default: break;
