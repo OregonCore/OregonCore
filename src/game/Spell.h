8cf714aec2f7e106196536f6e4078ee5815ffcc6
stfx <stfx@hotmail.de>
2011-02-20 14:13:49 +0100

    Re-add hacky crashfix for Spell::DoAllEffectOnTarget until we find the proper fix :)

--- src/game/Spell.h
+++ src/game/Spell.h
@@ -25,6 +25,8 @@
 
 #include "GridDefines.h"
 
+#define MAX_SPELL_ID    60000
+
 class Unit;
 class Player;
 class GameObject;
@@ -540,11 +542,13 @@ class Spell
             SpellMissInfo reflectResult:8;
             uint8  effectMask:8;
             bool   processed:1;
+            bool   deleted:1;
             int32  damage;
             bool   crit;
         };
         std::list<TargetInfo> m_UniqueTargetInfo;
         uint8 m_needAliveTargetMask;                        // Mask req. alive targets
+        bool m_destroyed;
 
         struct GOTargetInfo
         {
@@ -552,6 +556,7 @@ class Spell
             uint64 timeDelay;
             uint8  effectMask:8;
             bool   processed:1;
+            bool   deleted:1;
         };
         std::list<GOTargetInfo> m_UniqueGOTargetInfo;
 
--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -330,8 +330,13 @@ void Spell::SpellDamageSchoolDmg(uint32 effect_idx)
                 {
                     uint32 count = 0;
                     for (std::list<TargetInfo>::iterator ihit= m_UniqueTargetInfo.begin(); ihit != m_UniqueTargetInfo.end(); ++ihit)
+                    {
+                        if (ihit->deleted)
+                            continue;
+
                         if (ihit->effectMask & (1<<effect_idx))
                             ++count;
+                    }
 
                     damage /= count;                    // divide to all targets
                 }
@@ -680,8 +685,13 @@ void Spell::EffectDummy(uint32 i)
                 {
                     uint32 count = 0;
                     for (std::list<TargetInfo>::iterator ihit= m_UniqueTargetInfo.begin(); ihit != m_UniqueTargetInfo.end(); ++ihit)
+                    {
+                        if (ihit->deleted)
+                            continue;
+
                         if (ihit->effectMask & (1<<i))
                             ++count;
+                    }
 
                     damage = 12000; // maybe wrong value
                     damage /= count;
@@ -690,11 +700,16 @@ void Spell::EffectDummy(uint32 i)
 
                      // now deal the damage
                     for (std::list<TargetInfo>::iterator ihit= m_UniqueTargetInfo.begin(); ihit != m_UniqueTargetInfo.end(); ++ihit)
+                    {
+                        if (ihit->deleted)
+                            continue;
+
                         if (ihit->effectMask & (1<<i))
                         {
                             if (Unit* casttarget = Unit::GetUnit((*unitTarget), ihit->targetGUID))
                                 m_caster->DealDamage(casttarget, damage, NULL, SPELL_DIRECT_DAMAGE, SPELL_SCHOOL_MASK_ARCANE, spellInfo, false);
                         }
+                    }
 
                     return;
                 }
@@ -1702,7 +1717,12 @@ void Spell::EffectDummy(uint32 i)
                     // Righteous Defense (step 2) (in old version 31980 dummy effect)
                     // Clear targets for eff 1
                     for (std::list<TargetInfo>::iterator ihit= m_UniqueTargetInfo.begin();ihit != m_UniqueTargetInfo.end();++ihit)
+                    {
+                        if (ihit->deleted)
+                            continue;
+
                         ihit->effectMask &= ~(1<<1);
+                    }
 
                     // not empty (checked)
                     Unit::AttackerSet const& attackers = unitTarget->getAttackers();
