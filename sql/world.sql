e27ae8f3ef460bcf611526113a029f6e7a647462
desteny <evo_racer@web.de>
2011-05-30 01:51:20 +0200

    Insert missing World Table

--- sql/world.sql
+++ sql/world.sql
@@ -11155,6 +11155,31 @@ LOCK TABLES `script_texts` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `script_waypoint`
+--
+
+DROP TABLE IF EXISTS script_waypoint;
+CREATE TABLE `script_waypoint` (
+  `entry` mediumint(8) unsigned NOT NULL DEFAULT '0' COMMENT 'creature_template `entry`',
+  pointid mediumint(8) unsigned NOT NULL DEFAULT '0',
+  location_x float NOT NULL DEFAULT '0',
+  location_y float NOT NULL DEFAULT '0',
+  location_z float NOT NULL DEFAULT '0',
+  waittime int(10) unsigned NOT NULL DEFAULT '0' COMMENT 'waittime in millisecs',
+  point_comment text,
+  PRIMARY KEY (entry, pointid)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='Script Creature waypoints';
+
+--
+-- Dumping data for table `script_texts`
+--
+
+LOCK TABLES `script_waypoint` WRITE;
+/*!40000 ALTER TABLE `script_waypoint` DISABLE KEYS */;
+/*!40000 ALTER TABLE `script_waypoint` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `skill_discovery_template`
 --
 
