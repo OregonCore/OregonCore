61b82687b7e41f725b0edfbf3e15f05e9a15368d
desteny <evo_racer@web.de>
2011-09-14 03:22:44 +0200

    Update Config Version for Last Updates

--- src/oregoncore/Main.cpp
+++ src/oregoncore/Main.cpp
@@ -38,7 +38,7 @@
 // Format is YYYYMMDDRR where RR is the change in the conf file
 // for that day.
 #ifndef _OREGON_CORE_CONFVER
-# define _OREGON_CORE_CONFVER 2011020301
+# define _OREGON_CORE_CONFVER 2011091401
 #endif //_OREGON_CORE_CONFVER
 
 #ifdef _WIN32
