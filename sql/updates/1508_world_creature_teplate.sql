6b4149bb9ee716608973b15e3f7c082bc66729d0
henhouse <henry925@gmail.com>
2011-12-13 07:36:57 -0500

    Oops, remember to remove all the SQL that we merged.

--- sql/updates/1508_world_creature_teplate.sql
+++
@@ -1,2 +0,0 @@
-DELETE FROM spell_script_target WHERE entry = '33655';
-INSERT INTO spell_script_target VALUES (33655, 1, 19291),(33655, 1, 19292);
--- sql/updates/1509_world_creature_template.sql
+++
@@ -1,4 +0,0 @@
-UPDATE creature_template SET KillCredit1 = '21161' WHERE entry = '18952';
-UPDATE creature_template SET KillCredit1 = '21161' WHERE entry = '16876';
-UPDATE creature_template SET KillCredit1 = '21161' WHERE entry = '19701';
-UPDATE creature_template SET KillCredit1 = '21161' WHERE entry = '16925';
