a0ea0a9f20ceb0f875c0f9961e0edb55201334c4
zlokobni <none@none>
2011-12-13 12:04:59 +0200

    Fixed quest 10482.

---
+++ sql/updates/1509_world_creature_template.sql
@@ -0,0 +1,4 @@
+UPDATE creature_template SET KillCredit1 = '21161' WHERE entry = '18952';
+UPDATE creature_template SET KillCredit1 = '21161' WHERE entry = '16876';
+UPDATE creature_template SET KillCredit1 = '21161' WHERE entry = '19701';
+UPDATE creature_template SET KillCredit1 = '21161' WHERE entry = '16925';
