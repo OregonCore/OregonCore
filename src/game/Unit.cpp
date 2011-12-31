6b004c21cbea9a9d84f700be89f6d94369512321
henhouse <henry925@gmail.com>
2011-12-30 19:54:29 -0500

    Clear threat list on units when they die (fix players having aggro on corpses)

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -9164,7 +9164,7 @@ bool Unit::CanHaveThreatList() const
         return false;
 
     // only alive units can have threat list
-    if (!isAlive())
+    if (!isAlive() || isDying())
         return false;
 
     // totems can not have threat list
--- src/game/Unit.h
+++ src/game/Unit.h
@@ -1132,6 +1132,7 @@ class Unit : public WorldObject
         virtual void MoveOutOfRange(Player &) {  };
 
         bool isAlive() const { return (m_deathState == ALIVE); };
+        bool isDying() const { return (m_deathState == JUST_DIED); };
         bool isDead() const { return (m_deathState == DEAD || m_deathState == CORPSE); };
         DeathState getDeathState() { return m_deathState; };
         virtual void setDeathState(DeathState s);           // overwrited in Creature/Player/Pet
