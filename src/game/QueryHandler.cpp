61f135404a4142e2038c65d4a3aafbd892e7355c
henhouse <henry925@gmail.com>
2012-02-13 18:49:03 -0500

    Add missing data for gameobject query. Based on data from MaNGOS-One. This value was added in 2.4.2 or 2.4.3    https://github.com/mangos-one/server/issues/19

--- src/game/QueryHandler.cpp
+++ src/game/QueryHandler.cpp
@@ -240,6 +240,7 @@ void WorldSession::HandleGameObjectQueryOpcode(WorldPacket & recv_data)
         data << CastBarCaption;                             // 2.0.3, string. Text will appear in Cast Bar when using GO (ex: "Collecting")
         data << uint8(0);                                   // 2.0.3, string
         data.append(info->raw.data, 24);
+        data << float(info->size);                          // go size
         SendPacket(&data);
         sLog.outDebug("WORLD: Sent SMSG_GAMEOBJECT_QUERY_RESPONSE");
     }
