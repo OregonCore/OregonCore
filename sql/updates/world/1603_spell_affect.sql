d2459e8c8a5a380bdd0304a8c8a2489d2f7305cf
desteny <evo_racer@web.de>
2012-03-14 00:20:34 +0100

    Sanctified Seals now it affects Seal of the Crusader too. As it should. *Thanks Trentone

---
+++ sql/updates/world/1603_spell_affect.sql
@@ -0,0 +1,3 @@
+UPDATE spell_affect SET SpellFamilyMask=4398214283776 WHERE entry=32043 AND effectId=1;
+UPDATE spell_affect SET SpellFamilyMask=4398214283776 WHERE entry=35396 AND effectId=1;
+UPDATE spell_affect SET SpellFamilyMask=4398214283776 WHERE entry=35397 AND effectId=1;
