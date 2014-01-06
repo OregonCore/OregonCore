bb02ee8966d3359b3ac426baa6c03ff9001b773a
Yooki <none@none>
2014-01-06 01:07:08 +0100

    Updated AI Scripts:
    These are some entries I forgot in my last fix.
    I also corrected some unit names in the comment field.

--- sql/updates/world/1644_creature_ai_scripts.sql
+++ sql/updates/world/1644_creature_ai_scripts.sql
@@ -1,16 +1,35 @@
 --
--- Listed entries will now properly self-cast spells on death
+-- Listed entries will now properly self-cast spells on death.
+-- Also corrected some unit names in the comment field.
 --
 
 -- Cursed Mage - Cast Tainted Mind on Death --
 
 UPDATE `world`.`creature_ai_scripts` SET `action1_param2`=0 WHERE `id`=852410;
 
--- Spore Bat - Cast Energizing Spores on Death -- 
+-- Young Sporebat - Cast Energizing Spores on Death --
+
+UPDATE `world`.`creature_ai_scripts` SET `action1_param2`=0 WHERE  `id`=2038701;
+UPDATE `world`.`creature_ai_scripts` SET `comment`='Young Sporebat - Cast Energizing Spores on Death' WHERE  `id`=2038701;
+
+-- Sporebat - Cast Energizing Spores on Death -- 
 
 UPDATE `world`.`creature_ai_scripts` SET `action1_param2`=0 WHERE `id`=1812801;
+UPDATE `world`.`creature_ai_scripts` SET `comment`='Sporebat - Cast Energizing Spores on Death' WHERE  `id`=1812801;
+UPDATE `world`.`creature_ai_scripts` SET `comment`='Sporebat - Cast Spore Cloud' WHERE  `id`=1812802;
 
--- Greater Spore Bat - Cast Energizing Spores on Death --
+-- Greater Sporebat - Cast Energizing Spores on Death --
 
 UPDATE `world`.`creature_ai_scripts` SET `action1_param2`=0 WHERE `id`=1812901;
+UPDATE `world`.`creature_ai_scripts` SET `comment`='Greater Sporebat - Cast Energizing Spores on Death' WHERE  `id`=1812901;
+UPDATE `world`.`creature_ai_scripts` SET `comment`='Greater Sporebat - Cast Spore Cloud' WHERE  `id`=1812902;
 
+-- Sporewing - Cast Energizing Spores on Death --
+
+UPDATE `world`.`creature_ai_scripts` SET `action1_param2`=0 WHERE  `id`=1828001;
+
+
+-- Correct other incorrect named units in comment field --
+
+UPDATE `world`.`creature_template` SET `name`='Toxic Sporebat' WHERE  `entry`=22140;
+UPDATE `world`.`creature_template` SET `name`='Sporeggar Sporebat' WHERE  `entry`=22085;
