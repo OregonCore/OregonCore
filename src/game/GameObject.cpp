09da0ca6d12af0161eb694fff98a4e07a05ddb30
LordUsagi <lovelessusagi@gmail.com>
2014-01-31 14:19:29 -0400

    [Instances/Summoning] Allow meeting stones to summon members                       of the entire raid. Not just members of                       your immediate party.

--- src/game/GameObject.cpp
+++ src/game/GameObject.cpp
@@ -1265,7 +1265,7 @@ void GameObject::Use(Unit* user)
             Player* targetPlayer = ObjectAccessor::FindPlayer(player->GetSelection());
 
             // accept only use by player from same group for caster except caster itself
-            if (!targetPlayer || targetPlayer == player || !targetPlayer->IsInSameGroupWith(player))
+            if (!targetPlayer || targetPlayer == player || !targetPlayer->IsInSameRaidWith(player))
                 return;
 
             //required lvl checks!
