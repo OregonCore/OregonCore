4d13a8d35400e88bcc4d61702303521f5751eb5e
LordUsagi <lovelessusagi@gmail.com>
2014-02-09 22:11:05 -0400

    [Core/Pets] Fix pets moving from stay during combat

--- src/game/PetAI.cpp
+++ src/game/PetAI.cpp
@@ -464,7 +464,7 @@ bool PetAI::_CanAttack(Unit *target)
 
     // Stay - can attack if target is within range or commanded to
     if (me->GetCharmInfo()->HasCommandState(COMMAND_STAY))
-        return (me->IsWithinMeleeRange(target, MIN_MELEE_REACH) || me->GetCharmInfo()->IsCommandAttack());
+        return (me->IsWithinMeleeRange(target, MELEE_RANGE) || me->GetCharmInfo()->IsCommandAttack());
 
     // Follow
     if (me->GetCharmInfo()->HasCommandState(COMMAND_FOLLOW))
