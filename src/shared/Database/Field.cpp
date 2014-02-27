64df887a0659db517266ff8ed5c920cf8ace9027
LordUsagi <lovelessusagi@gmail.com>
2014-02-27 01:50:18 -0400

    [Core/Database] Optimize memory usage of Field class used in DB code.

--- src/shared/Database/Field.cpp
+++ src/shared/Database/Field.cpp
@@ -15,55 +15,6 @@
  *
  * You should have received a copy of the GNU General Public License along
  * with this program. If not, see <http://www.gnu.org/licenses/>.
- */
+ 
 
-#include "DatabaseEnv.h"
-
-Field::Field() :
-mValue(NULL), mType(DB_TYPE_UNKNOWN)
-{
-}
-
-Field::Field(Field &f)
-{
-    const char *value;
-
-    value = f.GetString();
-
-    if (value && (mValue = new char[strlen(value) + 1]))
-        strcpy(mValue, value);
-    else
-        mValue = NULL;
-
-    mType = f.GetType();
-}
-
-Field::Field(const char *value, enum Field::DataTypes type) :
-mType(type)
-{
-    if (value && (mValue = new char[strlen(value) + 1]))
-        strcpy(mValue, value);
-    else
-        mValue = NULL;
-}
-
-Field::~Field()
-{
-    if (mValue)
-        delete [] mValue;
-}
-
-void Field::SetValue(const char *value)
-{
-    if (mValue)
-        delete [] mValue;
-
-    if (value)
-    {
-        mValue = new char[strlen(value) + 1];
-        strcpy(mValue, value);
-    }
-    else
-        mValue = NULL;
-}
-
+#include "DatabaseEnv.h"*/
