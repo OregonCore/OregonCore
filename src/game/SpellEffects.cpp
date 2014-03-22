b209c7aa30557d7a263951efdf299811310b72c4
LordUsagi <lovelessusagi@gmail.com>
2014-03-22 10:43:14 -0300

    [Core/Spells] Implement ScriptEffect for spell 26678. Thanks tibbi.

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -4839,6 +4839,32 @@ void Spell::EffectScriptEffect(uint32 effIndex)
 
                     return;
                 }
+                case 26678:                                 // Heart Candy
+                {
+                    uint32 item = 0;
+                    switch (urand(0, 7))
+                    {
+                        case 0:
+                            item = 21816; break;
+                        case 1:
+                            item = 21817; break;
+                        case 2:
+                            item = 21818; break;
+                        case 3:
+                            item = 21819; break;
+                        case 4:
+                            item = 21820; break;
+                        case 5:
+                            item = 21821; break;
+                        case 6:
+                            item = 21822; break;
+                        case 7:
+                            item = 21823; break;
+                    }
+                    if (item)
+                        DoCreateItem(effIndex,item);
+                    break;
+                }
                 // Mirren's Drinking Hat
                 case 29830:
                 {
