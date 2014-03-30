dc4621e3967ffdac5bcaac18e10de8eaa7d77a85
Zaffy <zaffy@lunar.sk>
2014-03-30 18:42:23 +0200

    Add missing sql files
    --HG--
    branch : devel

---
+++ sql/updates/realmd/1617_refer_a_friend.sql
@@ -0,0 +1,10 @@
+drop table if exists account_referred;
+create table account_referred
+(
+    `id1` bigint unsigned not null comment 'Referring Account',
+    `id2` bigint unsigned not null comment 'Referred Account',
+
+    PRIMARY KEY `id1` (`id`),
+    KEY `id2` (`id2`)
+
+) engine=InnoDB row_format=fixed COMMENT='Refer-A-Friend';
