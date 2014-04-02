be6dd4726766d291c5ae8b855ac6a87cca757f6c
LordUsagi <lovelessusagi@gmail.com>
2014-04-02 18:40:38 -0300

    [DB/Typos] Fix a typo in a recent SQL file. Thanks to smoldar.

--- sql/updates/world/1917_spell_cooldowns.sql
+++ sql/updates/world/1917_spell_cooldowns.sql
@@ -1,5 +1,5 @@
 CREATE TABLE spell_cooldown (
   spellid int(11) unsigned NOT NULL default '0',
-  cooldown int(11) NOT NULL default '',
+  cooldown int(11) DEFAULT NULL,
   KEY spellid (spellid)
 );
