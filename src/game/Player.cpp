8733555cc826dde8a181f5bff0638e9f11bb53d4
LordUsagi <lovelessusagi@gmail.com>
2014-07-27 03:06:26 -0300

    [Core/Auras] Solve exploit involving SPELL_AURA_MOD_DAMAGE_DONE and weapon swapp              ing.
                 Thanks to trisjdc.

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -7234,6 +7234,13 @@ void Player::_ApplyWeaponDependentAuraDamageMod(Item *item, WeaponAttackType att
     if (item->IsFitToSpellRequirements(aura->GetSpellProto()))
     {
         HandleStatModifier(unitMod, unitModType, float(aura->GetModifierValue()),apply);
+        if (unitModType == TOTAL_VALUE)
+        {
+            if (aura->GetAmount() > 0)
+                ApplyModUInt32Value(PLAYER_FIELD_MOD_DAMAGE_DONE_POS, aura->GetModifierValue(), apply);
+            else
+                ApplyModUInt32Value(PLAYER_FIELD_MOD_DAMAGE_DONE_NEG, aura->GetModifierValue(), apply);
+        }
     }
 }
 
