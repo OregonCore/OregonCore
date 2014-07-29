44b65d27c11b7a2458d422814bcb23c53e366f5b
LordUsagi <lovelessusagi@gmail.com>
2014-07-29 14:31:03 -0300

    Fix recent compile errors with proper values.

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -7236,7 +7236,7 @@ void Player::_ApplyWeaponDependentAuraDamageMod(Item *item, WeaponAttackType att
         HandleStatModifier(unitMod, unitModType, float(aura->GetModifierValue()),apply);
         if (unitModType == TOTAL_VALUE)
         {
-            if (aura->GetStackAmount() > 0)
+            if (aura->GetAmount() > 0)
                 ApplyModUInt32Value(PLAYER_FIELD_MOD_DAMAGE_DONE_POS, aura->GetModifierValue(), apply);
             else
                 ApplyModUInt32Value(PLAYER_FIELD_MOD_DAMAGE_DONE_NEG, aura->GetModifierValue(), apply);
