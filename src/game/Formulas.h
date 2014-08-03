10e0d470324cbe4b669a3e17e7c67f0335a07cd3
LordUsagi <lovelessusagi@gmail.com>
2014-08-03 03:35:33 -0300

    [Cleanup] Remove useless WotLK todo note

--- src/game/Formulas.h
+++ src/game/Formulas.h
@@ -79,7 +79,6 @@ namespace Oregon
 
         inline uint32 BaseGain(uint32 pl_level, uint32 mob_level, ContentLevels content)
         {
-            //@todo need modifier for CONTENT_71_80 different from CONTENT_61_70?
             const uint32 nBaseExp = content == CONTENT_1_60 ? 45 : 235;
             if (mob_level >= pl_level)
             {
