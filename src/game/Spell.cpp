133b0e40b68eb1b8adff814e94397b35e781ccc9
LordUsagi <lovelessusagi@gmail.com>
2014-10-06 14:56:31 -0300

    [Core/Spells] Fix evades causing combat and allow hostile dispels to cause combat.

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -1036,7 +1036,7 @@ void Spell::DoAllEffectOnTarget(TargetInfo *target)
             m_caster->ToPlayer()->CastedCreatureOrGO(unit->GetEntry(),unit->GetGUID(),m_spellInfo->Id);
     }
 
-    if (!m_caster->IsFriendlyTo(unit) && !IsPositiveSpell(m_spellInfo->Id))
+    if (missInfo != SPELL_MISS_EVADE && m_caster && !m_caster->IsFriendlyTo(unit) && (!IsPositiveSpell(m_spellInfo->Id) || m_spellInfo->HasEffect(SPELL_EFFECT_DISPEL)))
     {
         m_caster->CombatStart(unit, !(m_spellInfo->AttributesEx3 & SPELL_ATTR_EX3_NO_INITIAL_AGGRO));
 
