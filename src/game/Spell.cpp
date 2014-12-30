c85bb820ce7e12fac1dcf11d83e88ea2315bd0be
Zaffy <zaffy@lunar.sk>
2014-12-30 15:08:39 +0100

    Fix broken mounting in bgs & in some dungeons

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -4625,7 +4625,7 @@ SpellCastResult Spell::CheckCast(bool strict)
                 InstanceTemplate const* it = sObjectMgr.GetInstanceTemplate(m_caster->GetMapId());
                 if (it)
                     AllowMount = it->allowMount;
-                if (m_caster->GetTypeId() == TYPEID_PLAYER && !AllowMount && !m_IsTriggeredSpell && !m_spellInfo->AreaId)
+                if (m_caster->GetTypeId() == TYPEID_PLAYER && !AllowMount && !m_IsTriggeredSpell && m_spellInfo->AreaId)
                     return SPELL_FAILED_NO_MOUNTS_ALLOWED;
 
                 //if (m_caster->GetAreaId() == 35)
