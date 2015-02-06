adc657ecfacb1997a451ac62fdf05564f598a67d
Zaffy <zaffy@lunar.sk>
2015-02-06 23:20:31 +0100

    Make talent "Clever Traps" affect all traps it should

---
+++ sql/updates/world/2546_clever_traps.sql
@@ -0,0 +1,5 @@
+delete from spell_affect where entry = 19245;
+
+insert into spell_affect (entry, effectId, SpellFamilyFlags)
+values (19245, 0, 24), -- freezing trap & frost trap
+values (19245, 1, 35184372088836); -- immolation, explosive & snake trap
