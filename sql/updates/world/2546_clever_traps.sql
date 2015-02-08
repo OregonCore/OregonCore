25a915e0b684c53029b8bbb99d48998e63409e54
smoldar <pavelobolecky@seznam.cz>
2015-02-08 00:20:51 +0000

    2546_clever_traps.sql edited online with Bitbucket
    --HG--
    branch : smoldar/2546_clever_trapssql-edited-online-with--1423354838645

--- sql/updates/world/2546_clever_traps.sql
+++ sql/updates/world/2546_clever_traps.sql
@@ -2,4 +2,4 @@ delete from spell_affect where entry = 19245;
 
 insert into spell_affect (entry, effectId, SpellFamilyFlags)
 values (19245, 0, 24), -- freezing trap & frost trap
-values (19245, 1, 35184372088836); -- immolation, explosive & snake trap
+(19245, 1, 35184372088836); -- immolation, explosive & snake trap
