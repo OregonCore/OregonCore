b505827d63c1c8976ddcb6082ce84af4d65ce5bf
LordUsagi <lovelessusagi@gmail.com>
2015-04-19 11:08:56 -0300

    [Core/Creatures] Cleanup setDeathState. Remove duplicate code. Remove virtual mount                  on death.

--- src/game/Creature.cpp
+++ src/game/Creature.cpp
@@ -1465,7 +1465,6 @@ void Creature::setDeathState(DeathState s)
         SetNoSearchAssistance(false);
 
         //Dismiss group if is leader
-        //Dismiss group if is leader
         if (m_formation && m_formation->getLeader() == this)
             m_formation->FormationReset(true);
 
@@ -1476,12 +1475,11 @@ void Creature::setDeathState(DeathState s)
             m_zoneScript->OnCreatureDeath(this);
 
         SetUInt64Value (UNIT_FIELD_TARGET, 0);              // remove target selection in any cases (can be set at aura remove in Unit::setDeathState)
-        SetUInt32Value(UNIT_NPC_FLAGS, 0);
+        SetUInt32Value(UNIT_NPC_FLAGS, UNIT_NPC_FLAG_NONE);
+
+        SetUInt32Value(UNIT_FIELD_MOUNTDISPLAYID, 0);       // if creature is mounted on a virtual mount, remove it at death
 
         setActive(false);
-
-        if (canFly() && FallGround())
-            return;
 
         // return, since we promote to DEAD_FALLING. DEAD_FALLING is promoted to CORPSE at next update.
         if (canFly() && FallGround())
