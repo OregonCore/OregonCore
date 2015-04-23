ac81ddcdd294b7a8a962e4906a70bda1e5fb5c8c
LordUsagi <lovelessusagi@gmail.com>
2015-04-23 00:29:50 -0300

    [Scripts/Quests] Implement script support for quests:                     - The Scent of Lar'korwi (4291)                     - The Ancient Leaf (7632)
                     Thanks to ScriptDev2.

---
+++ sql/updates/world/2639_areatrigger_scripts.sql
@@ -0,0 +1,21 @@
+DELETE FROM areatrigger_scripts WHERE entry=3587;
+INSERT INTO areatrigger_scripts VALUES
+(3587,'at_ancient_leaf');
+
+DELETE FROM areatrigger_scripts WHERE entry BETWEEN 1726 AND 1740;
+INSERT INTO areatrigger_scripts VALUES
+(1726,'at_scent_larkorwi'),
+(1727,'at_scent_larkorwi'),
+(1728,'at_scent_larkorwi'),
+(1729,'at_scent_larkorwi'),
+(1730,'at_scent_larkorwi'),
+(1731,'at_scent_larkorwi'),
+(1732,'at_scent_larkorwi'),
+(1733,'at_scent_larkorwi'),
+(1734,'at_scent_larkorwi'),
+(1735,'at_scent_larkorwi'),
+(1736,'at_scent_larkorwi'),
+(1737,'at_scent_larkorwi'),
+(1738,'at_scent_larkorwi'),
+(1739,'at_scent_larkorwi'),
+(1740,'at_scent_larkorwi');
--- src/scripts/World/areatrigger_scripts.cpp
+++ src/scripts/World/areatrigger_scripts.cpp
@@ -26,6 +26,8 @@ EndScriptData */
 at_coilfang_waterfall           4591
 at_legion_teleporter            4560 Teleporter TO Invasion Point: Cataclysm
 at_ravenholdt
+at_scent_larkorwi               1726,1727,1728,1729,1730,1731,1732,1733,1734,1735,1736,1737,1738,1739,1740
+at_ancient_leaf                 3587
 EndContentData */
 
 #include "ScriptPCH.h"
@@ -96,6 +98,74 @@ bool AreaTrigger_at_ravenholdt(Player* pPlayer, const AreaTriggerEntry* /*pAt*/)
     return false;
 }
 
+/*######
+## at_scent_larkorwi
+######*/
+
+enum eLarkorwi
+{
+    QUEST_SCENT_OF_LARKORWI     = 4291,
+    NPC_LARKORWI_MATE           = 9683
+};
+
+bool AreaTrigger_at_scent_larkorwi(Player* player, AreaTriggerEntry const* at)
+{
+    if (player->IsAlive() && !player->isGameMaster() && player->GetQuestStatus(QUEST_SCENT_OF_LARKORWI) == QUEST_STATUS_INCOMPLETE)
+    {
+        if (!GetClosestCreatureWithEntry(player, NPC_LARKORWI_MATE, 25.0f, false))
+            player->SummonCreature(NPC_LARKORWI_MATE, at->x, at->y, at->z, 3.3f, TEMPSUMMON_TIMED_DESPAWN_OUT_OF_COMBAT, 2 * MINUTE * IN_MILLISECONDS);
+    }
+
+    return false;
+}
+
+/*######
+## at_ancient_leaf
+######*/
+
+enum eAncientLeaf
+{
+    QUEST_ANCIENT_LEAF              = 7632,
+
+    NPC_VARTRUS                     = 14524,
+    NPC_STOMA                       = 14525,
+    NPC_HASTAT                      = 14526,
+
+    MAX_ANCIENTS                    = 3
+};
+
+struct AncientSpawn
+{
+    uint32 uiEntry;
+    float fX, fY, fZ, fO;
+};
+
+static const AncientSpawn afSpawnLocations[MAX_ANCIENTS] =
+{
+    { NPC_VARTRUS, 6204.051758f, -1172.575684f, 370.079224f, 0.86052f },    // Vartus the Ancient
+    { NPC_STOMA,   6246.953613f, -1155.985718f, 366.182953f, 2.90269f },    // Stoma the Ancient
+    { NPC_HASTAT,  6193.449219f, -1137.834106f, 366.260529f, 5.77332f },    // Hastat the Ancient
+};
+
+bool AreaTrigger_at_ancient_leaf(Player* player, AreaTriggerEntry const* /*at*/)
+{
+    if (player->isGameMaster() || !player->IsAlive())
+        return false;
+
+    // Handle Call Ancients event start - The area trigger summons 3 ancients
+    if (player->GetQuestStatus(QUEST_ANCIENT_LEAF) == QUEST_STATUS_COMPLETE)
+    {
+        // If ancients are already spawned, skip the rest
+        if (GetClosestCreatureWithEntry(player, NPC_VARTRUS, 50.0f) || GetClosestCreatureWithEntry(player, NPC_STOMA, 50.0f) || GetClosestCreatureWithEntry(player, NPC_HASTAT, 50.0f))
+            return true;
+
+        for (uint8 i = 0; i < MAX_ANCIENTS; ++i)
+            player->SummonCreature(afSpawnLocations[i].uiEntry, afSpawnLocations[i].fX, afSpawnLocations[i].fY, afSpawnLocations[i].fZ, afSpawnLocations[i].fO, TEMPSUMMON_TIMED_DESPAWN, 5 * MINUTE * IN_MILLISECONDS);
+    }
+
+    return false;
+}
+
 void AddSC_areatrigger_scripts()
 {
     Script* newscript;
@@ -113,5 +183,15 @@ void AddSC_areatrigger_scripts()
     newscript = new Script;
     newscript->Name = "at_ravenholdt";
     newscript->pAreaTrigger = &AreaTrigger_at_ravenholdt;
+    newscript->RegisterSelf();
+
+    newscript = new Script;
+    newscript->Name = "at_scent_larkorwi";
+    newscript->pAreaTrigger = &AreaTrigger_at_scent_larkorwi;
+    newscript->RegisterSelf();
+
+    newscript = new Script;
+    newscript->Name = "at_ancient_leaf";
+    newscript->pAreaTrigger = &AreaTrigger_at_ancient_leaf;
     newscript->RegisterSelf();
 }
