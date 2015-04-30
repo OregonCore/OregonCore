d3becc58699de8c21994ece538ac227526eae055
Zaffy <zaffy@lunar.sk>
2015-04-30 20:11:59 +0200

    Fix errors in sql updates

--- sql/updates/world/1949_ip2nation.sql
+++ sql/updates/world/1949_ip2nation.sql
@@ -8,9 +8,16 @@ WHERE table_schema = DATABASE()
     AND table_type = 'BASE TABLE'
     AND table_name = 'ip2nationcountries';
 
-SET @query = If(@exists>0,
+SELECT Count(*)
+INTO @alreadyRenamed
+FROM information_schema.tables 
+WHERE table_schema = DATABASE()
+    AND table_type = 'BASE TABLE'
+    AND table_name = 'ip2nationCountries';
+
+SET @query = If(@exists AND !@alreadyRenamed,
     'RENAME TABLE ip2nationcountries TO ip2nationCountries',
-    'SELECT \'nothing to rename\' status');
+    'SELECT \'nothing to rename or already renamed\' status');
 
 PREPARE stmt FROM @query;
 
