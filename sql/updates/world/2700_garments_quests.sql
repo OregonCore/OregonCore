ad85351aaa247a7bcf3edcf5d57723171b3bcdd3
Zaffy <zaffy@lunar.sk>
2015-05-15 19:06:59 +0200

    Fix a typo

--- sql/updates/world/2700_garments_quests.sql
+++ sql/updates/world/2700_garments_quests.sql
@@ -1,1 +1,1 @@
-update creature_template set unit_flags = 37376, RegenHealth = 0 where entry = in (12423, 12427, 12428, 12429, 12430);
+update creature_template set unit_flags = 37376, RegenHealth = 0 where entry in (12423, 12427, 12428, 12429, 12430);
