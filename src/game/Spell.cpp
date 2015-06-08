3b08aafb0a4826a8e31686e1aea7ea9d7f655268
LordUsagi <lovelessusagi@gmail.com>
2015-06-08 13:07:48 -0300

    [Core/Spells] Instead of checking for an existing shapeshift form, check for               shapeshift changing model. This allows levitate/water walking effects               to be cast on shadowform and stances.
                  Fixes #992

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -4649,10 +4649,9 @@ SpellCastResult Spell::CheckCast(bool strict)
                 if (m_targets.getUnitTarget()->GetTypeId() == TYPEID_PLAYER)
                 {
                     Player const* player = static_cast<Player const*>(m_targets.getUnitTarget());
-                    ShapeshiftForm form = player->m_form;
 
                     // Player is not allowed to cast water walk on shapeshifted/mounted player 
-                    if (form != FORM_NONE || player->IsMounted())
+                    if (player->HasShapeshiftChangingModel() || player->IsMounted())
                         return SPELL_FAILED_BAD_TARGETS;
                 }
 
