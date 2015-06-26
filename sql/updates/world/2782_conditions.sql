72a7b2685c361a2b5938af981bcf5c0c938da48c
Zaffy <zaffy@lunar.sk>
2015-06-26 19:10:31 +0200

    Fix mistake in sql

--- sql/updates/world/2782_conditions.sql
+++ sql/updates/world/2782_conditions.sql
@@ -1,4 +1,4 @@
 -- Execute
-DELETE FROM spell_dummy_condition WHERE entry IN (5308, 20658, 20660, 20661, 20662, 25234, 25236)
+DELETE FROM spell_dummy_condition WHERE entry IN (5308, 20658, 20660, 20661, 20662, 25234, 25236);
 INSERT INTO conditions VALUES
 (17, 0, 5308, 0, 0, 38, 1, 20, 4, 0, 0, 0, '', 'Execute requires target under or equal to 20% Health');
