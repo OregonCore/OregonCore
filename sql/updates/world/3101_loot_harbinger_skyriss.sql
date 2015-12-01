3bad792c364bd1d640aeb5fc019654e0907baab5
desteny <evo_racer@web.de>
2015-12-02 00:42:46 +0100

    Fix loot template for Harbinger Skyriss https://forums.oregon-core.net/viewtopic.php?id=390

---
+++ sql/updates/world/3101_loot_harbinger_skyriss.sql
@@ -0,0 +1,2 @@
+DELETE FROM `creature_loot_template` WHERE  `Entry`=21599 AND `Item`=43003;
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`) VALUES (21599, 35007, 35007, 66);
