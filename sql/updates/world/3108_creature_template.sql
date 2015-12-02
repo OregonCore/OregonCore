98530ee2f50b8b9dced6cf7efe2f706ba8bb2723
Raisken <r3isken@gmail.com>
2015-12-03 06:28:02 +0800

    Quest Prison Break DB fix

---
+++ sql/updates/world/3108_creature_template.sql
@@ -0,0 +1,8 @@
+UPDATE creature_template SET ScriptName = 'npc_ograbisi' WHERE creature_template.entry = 9677; 
+UPDATE creature_template SET ScriptName = 'npc_jaz' WHERE creature_template.entry = 9681; 
+UPDATE creature_template SET ScriptName = 'npc_shill' WHERE creature_template.entry = 9678; 
+UPDATE creature_template SET ScriptName = 'npc_marshal_windsor' WHERE creature_template.entry = 9023; 
+UPDATE creature_template SET ScriptName = 'npc_marshal_reginald_windsor' WHERE creature_template.entry = 9682; 
+UPDATE creature_template SET ScriptName = 'npc_tobias_seecher' WHERE creature_template.entry = 9679; 
+UPDATE creature_template SET ScriptName = 'npc_dughal_stormwing' WHERE creature_template.entry = 9022; 
+UPDATE creature_template SET ScriptName = 'npc_crest' WHERE creature_template.entry = 9680; 
