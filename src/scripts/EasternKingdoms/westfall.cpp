5586b8b7a54687dca58929dc3eca8940a68d101c
LordUsagi <lovelessusagi@gmail.com>
2015-12-24 20:01:59 -0400

    [DB/SAI] Script for quest Tome of Valor. Remove now unused script from core.          Fixes #857

--- src/scripts/EasternKingdoms/westfall.cpp
+++ src/scripts/EasternKingdoms/westfall.cpp
@@ -18,175 +18,17 @@
 /* ScriptData
 SDName: Westfall
 SD%Complete: 90
-SDComment: Quest support: 155, 1651
+SDComment: Quest support: 155
 SDCategory: Westfall
 EndScriptData */
 
 /* ContentData
-npc_daphne_stilwell
 npc_defias_traitor
 EndContentData */
 
 #include "ScriptMgr.h"
 #include "ScriptedCreature.h"
 #include "ScriptedEscortAI.h"
-
-/*######
-## npc_daphne_stilwell
-######*/
-
-enum eEnums
-{
-    SAY_DS_START        = -1000293,
-    SAY_DS_DOWN_1       = -1000294,
-    SAY_DS_DOWN_2       = -1000295,
-    SAY_DS_DOWN_3       = -1000296,
-    SAY_DS_PROLOGUE     = -1000297,
-
-    SPELL_SHOOT         = 6660,
-    QUEST_TOME_VALOR    = 1651,
-    NPC_DEFIAS_RAIDER   = 6180,
-    EQUIP_ID_RIFLE      = 20738
-};
-
-struct npc_daphne_stilwellAI : public npc_escortAI
-{
-    npc_daphne_stilwellAI(Creature* pCreature) : npc_escortAI(pCreature) {}
-
-    uint32 uiWPHolder;
-    uint32 uiShootTimer;
-
-    void Reset()
-    {
-        if (HasEscortState(STATE_ESCORT_ESCORTING))
-        {
-            switch (uiWPHolder)
-            {
-            case 7:
-                DoScriptText(SAY_DS_DOWN_1, me);
-                break;
-            case 8:
-                DoScriptText(SAY_DS_DOWN_2, me);
-                break;
-            case 9:
-                DoScriptText(SAY_DS_DOWN_3, me);
-                break;
-            }
-        }
-        else
-            uiWPHolder = 0;
-
-        uiShootTimer = 0;
-    }
-
-    void WaypointReached(uint32 uiPoint)
-    {
-        Player* pPlayer = GetPlayerForEscort();
-
-        if (!pPlayer)
-            return;
-
-        uiWPHolder = uiPoint;
-
-        switch (uiPoint)
-        {
-        case 4:
-            SetEquipmentSlots(false, EQUIP_NO_CHANGE, EQUIP_NO_CHANGE, EQUIP_ID_RIFLE);
-            me->SetSheath(SHEATH_STATE_RANGED);
-            me->HandleEmoteCommand(EMOTE_STATE_USESTANDING_NOSHEATHE);
-            break;
-        case 7:
-            me->SummonCreature(NPC_DEFIAS_RAIDER, -11450.836, 1569.755, 54.267, 4.230, TEMPSUMMON_TIMED_DESPAWN_OUT_OF_COMBAT, 30000);
-            me->SummonCreature(NPC_DEFIAS_RAIDER, -11449.697, 1569.124, 54.421, 4.206, TEMPSUMMON_TIMED_DESPAWN_OUT_OF_COMBAT, 30000);
-            me->SummonCreature(NPC_DEFIAS_RAIDER, -11448.237, 1568.307, 54.620, 4.206, TEMPSUMMON_TIMED_DESPAWN_OUT_OF_COMBAT, 30000);
-            break;
-        case 8:
-            me->SetSheath(SHEATH_STATE_RANGED);
-            me->SummonCreature(NPC_DEFIAS_RAIDER, -11450.836, 1569.755, 54.267, 4.230, TEMPSUMMON_TIMED_DESPAWN_OUT_OF_COMBAT, 30000);
-            me->SummonCreature(NPC_DEFIAS_RAIDER, -11449.697, 1569.124, 54.421, 4.206, TEMPSUMMON_TIMED_DESPAWN_OUT_OF_COMBAT, 30000);
-            me->SummonCreature(NPC_DEFIAS_RAIDER, -11448.237, 1568.307, 54.620, 4.206, TEMPSUMMON_TIMED_DESPAWN_OUT_OF_COMBAT, 30000);
-            me->SummonCreature(NPC_DEFIAS_RAIDER, -11448.037, 1570.213, 54.961, 4.283, TEMPSUMMON_TIMED_DESPAWN_OUT_OF_COMBAT, 30000);
-            break;
-        case 9:
-            me->SetSheath(SHEATH_STATE_RANGED);
-            me->SummonCreature(NPC_DEFIAS_RAIDER, -11450.836, 1569.755, 54.267, 4.230, TEMPSUMMON_TIMED_DESPAWN_OUT_OF_COMBAT, 30000);
-            me->SummonCreature(NPC_DEFIAS_RAIDER, -11449.697, 1569.124, 54.421, 4.206, TEMPSUMMON_TIMED_DESPAWN_OUT_OF_COMBAT, 30000);
-            me->SummonCreature(NPC_DEFIAS_RAIDER, -11448.237, 1568.307, 54.620, 4.206, TEMPSUMMON_TIMED_DESPAWN_OUT_OF_COMBAT, 30000);
-            me->SummonCreature(NPC_DEFIAS_RAIDER, -11448.037, 1570.213, 54.961, 4.283, TEMPSUMMON_TIMED_DESPAWN_OUT_OF_COMBAT, 30000);
-            me->SummonCreature(NPC_DEFIAS_RAIDER, -11449.018, 1570.738, 54.828, 4.220, TEMPSUMMON_TIMED_DESPAWN_OUT_OF_COMBAT, 30000);
-            break;
-        case 10:
-            SetRun(false);
-            break;
-        case 11:
-            DoScriptText(SAY_DS_PROLOGUE, me);
-            break;
-        case 13:
-            SetEquipmentSlots(true);
-            me->SetSheath(SHEATH_STATE_UNARMED);
-            me->HandleEmoteCommand(EMOTE_STATE_USESTANDING_NOSHEATHE);
-            break;
-        case 17:
-            pPlayer->GroupEventHappens(QUEST_TOME_VALOR, me);
-            break;
-        }
-    }
-
-    void AttackStart(Unit* pWho)
-    {
-        if (!pWho)
-            return;
-
-        if (me->Attack(pWho, false))
-        {
-            me->AddThreat(pWho, 0.0f);
-            me->SetInCombatWith(pWho);
-            pWho->SetInCombatWith(me);
-
-            me->GetMotionMaster()->MoveChase(pWho, 30.0f);
-        }
-    }
-
-    void JustSummoned(Creature* pSummoned)
-    {
-        pSummoned->AI()->AttackStart(me);
-    }
-
-    void Update(const uint32 diff)
-    {
-        npc_escortAI::UpdateAI(diff);
-
-        if (!UpdateVictim())
-            return;
-
-        if (uiShootTimer <= diff)
-        {
-            uiShootTimer = 1500;
-
-            if (!me->IsWithinDist(me->getVictim(), ATTACK_DISTANCE))
-                DoCastVictim( SPELL_SHOOT);
-        }
-        else uiShootTimer -= diff;
-    }
-};
-
-bool QuestAccept_npc_daphne_stilwell(Player* pPlayer, Creature* pCreature, const Quest* pQuest)
-{
-    if (pQuest->GetQuestId() == QUEST_TOME_VALOR)
-    {
-        DoScriptText(SAY_DS_START, pCreature);
-
-        if (npc_escortAI* pEscortAI = CAST_AI(npc_daphne_stilwellAI, pCreature->AI()))
-            pEscortAI->Start(true, true, pPlayer->GetGUID());
-    }
-
-    return true;
-}
-
-CreatureAI* GetAI_npc_daphne_stilwell(Creature* pCreature)
-{
-    return new npc_daphne_stilwellAI(pCreature);
-}
 
 /*######
 ## npc_defias_traitor
@@ -260,12 +102,6 @@ CreatureAI* GetAI_npc_defias_traitor(Creature* pCreature)
 void AddSC_westfall()
 {
     Script* newscript;
-
-    newscript = new Script;
-    newscript->Name = "npc_daphne_stilwell";
-    newscript->GetAI = &GetAI_npc_daphne_stilwell;
-    newscript->pQuestAccept = &QuestAccept_npc_daphne_stilwell;
-    newscript->RegisterSelf();
 
     newscript = new Script;
     newscript->Name = "npc_defias_traitor";
