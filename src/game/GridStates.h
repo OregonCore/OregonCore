bcd8fe995e8b85e36d8441222f42a3b9a8022879
LordUsagi <lovelessusagi@gmail.com>
2016-02-05 13:33:21 -0400

    Core/Cleanup: Cleanup GridStates

--- src/game/GridStates.h
+++ src/game/GridStates.h
@@ -24,28 +24,7 @@
 class GridState
 {
     public:
-        #ifdef OREGON_DEBUG
-#define MAGIC_TESTVAL 0xFBE823BA
-        GridState()
-        {
-            i_Magic = MAGIC_TESTVAL;
-        }
-        bool checkMagic()
-        {
-            if (i_Magic != MAGIC_TESTVAL)
-            {
-                sLog.outError("!!! GridState: Magic value gone !!!");
-                return false;
-            }
-            return true;
-        }
-        void setMagic()
-        {
-            i_Magic = MAGIC_TESTVAL;
-        }
-        unsigned int i_Magic;
-        #endif
-        virtual ~GridState() {}
+        virtual ~GridState() { };
         virtual void Update(Map &, NGridType&, GridInfo &, const uint32 t_diff) const = 0;
 };
 
@@ -53,28 +32,28 @@ class InvalidState : public GridState
 {
     public:
 
-        void Update(Map &, NGridType &, GridInfo &, const uint32 t_diff) const;
+        void Update(Map &, NGridType &, GridInfo &, const uint32 t_diff) const override;
 };
 
 class ActiveState : public GridState
 {
     public:
 
-        void Update(Map &, NGridType &, GridInfo &, const uint32 t_diff) const;
+        void Update(Map &, NGridType &, GridInfo &, const uint32 t_diff) const override;
 };
 
 class IdleState : public GridState
 {
     public:
 
-        void Update(Map &, NGridType &, GridInfo &, const uint32 t_diff) const;
+        void Update(Map &, NGridType &, GridInfo &, const uint32 t_diff) const override;
 };
 
 class RemovalState : public GridState
 {
     public:
 
-        void Update(Map &, NGridType &, GridInfo &, const uint32 t_diff) const;
+        void Update(Map &, NGridType &, GridInfo &, const uint32 t_diff) const override;
 };
 #endif
 
