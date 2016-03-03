e6aff446c956344dba8aa398743aa5b4b7b39610
Zaffy <zaffy@lunar.sk>
2016-03-03 17:06:13 +0100

    [Core/Build] Fix compile conflicts win/unix

--- src/collision/BoundingIntervalHierarchy.h
+++ src/collision/BoundingIntervalHierarchy.h
@@ -30,14 +30,12 @@
 #include <limits>
 #include <cmath>
 
-#ifdef __APPLE__
-  #define isnan(x) ( std::isnan(x) )
-#endif
-
 #define MAX_STACK_SIZE 64
 
 #ifdef _MSC_VER
     #define isnan(x) _isnan(x)
+#else
+    #define isnan(x) (std::isnan(x))
 #endif
 
 using G3D::Vector3;
