5bea9182c1053cf2f04c13bbe68e32c46a766227
Zaffy <zaffy@lunar.sk>
2016-03-12 18:50:01 +0100

    [Core] Fix an issue with revision.h and db updater
    Signed-off-by: kotishe

--- revision.h.in.cmake
+++ revision.h.in.cmake
@@ -4,6 +4,6 @@
  #define _HASH          "@rev_hash_str@"
  #define FILEVER        0,0,@rev_id@,0
  #define PRODUCTVER     0,0,@rev_id@,0
- #define STRFILEVER     "0, 0, @rev_id@, @rev_hash@"
- #define STRPRODUCTVER  "0, 0, @rev_id@, @rev_hash@"
+ #define STRFILEVER     "0.0.@rev_id@.@rev_hash_str@"
+ #define STRPRODUCTVER  "0.0.@rev_id@.@rev_hash_str@"
 #endif // __REVISION_H__
