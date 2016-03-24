9ff1ce8dcde30b87de8f13a4f2cc469cbc9f95fe
Zaffy <zaffy@lunar.sk>
2016-03-24 18:14:01 +0100

    [Core/Scripts] Add missing SQL assignements
    Some scripts are still missing though due to missing data.

---
+++ sql/updates/world/2016_03_24_creature_template_2.sql
@@ -0,0 +1,15 @@
+
+update creature_template set AIName = '', ScriptName = 'npc_skyguard_ace' where entry = 23377;
+update creature_template set AIName = '', ScriptName = 'npc_abyssal_flamebringer' where entry = 19973;
+update creature_template set AIName = '', ScriptName = 'npc_legion_flak_cannon' where entry = 23082;
+update creature_template set AIName = '', ScriptName = 'npc_aether_ray' where entry = 22181;
+update creature_template set AIName = '', ScriptName = 'npc_skulloc_soulgrinder' where entry = 22910;
+update creature_template set AIName = '', ScriptName = 'npc_bloodmaul_direwolf' where entry = 20058;
+update creature_template set AIName = '', ScriptName = 'npc_kolphis_darkscale' where entry = 22019;
+update creature_template set AIName = '', ScriptName = 'npc_evergrove_ancient' where entry = 22478;
+update creature_template set AIName = '', ScriptName = 'npc_rexxar' where entry = 22448;
+update creature_template set AIName = '', ScriptName = 'npc_searing_elemental' where entry = 20514;
+update creature_template set AIName = '', ScriptName = 'npc_dragonmaw_archer' where entry = 22251;
+
+update gameobject_template set AIName = '', ScriptName = 'go_apexis_monument' where entry = 185944;
+update gameobject_template set AIName = '', scriptName = 'go_ancient_skull_pile' where entry = 185928;
