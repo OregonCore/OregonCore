6469b9cd045a3b1db047c8dcaf31c633323751eb
LordUsagi <lovelessusagi@gmail.com>
2016-05-05 14:05:07 -0300

    Fix typo

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -9711,7 +9711,7 @@ float Unit::ApplyTotalThreatModifier(float fThreat, SpellSchoolMask schoolMask)
 void Unit::AddThreat(Unit* victim, float fThreat, SpellSchoolMask schoolMask, SpellEntry const* threatSpell)
 {
     // Only mobs can manage threat lists
-    if (CanHaveThreatList() || !HasUnitState(UNIT_STATE_EVADE))
+    if (CanHaveThreatList() && !HasUnitState(UNIT_STATE_EVADE))
         m_ThreatManager.addThreat(victim, fThreat, schoolMask, threatSpell);
 }
 
