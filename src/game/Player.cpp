56c9be4d77c00a6b4801418f9653513f81c6402e
LordUsagi <lovelessusagi@gmail.com>
2016-05-29 19:17:13 -0300

    Core/Quests: Remove wrong call to SendQuestComplete()
    Based on commit by SnapperRy @ TrinityCore

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -13394,12 +13394,8 @@ void Player::CompleteQuest(uint32 quest_id)
             SetQuestSlotState(log_slot, QUEST_STATE_COMPLETE);
 
         if (Quest const* qInfo = sObjectMgr.GetQuestTemplate(quest_id))
-        {
             if (qInfo->HasFlag(QUEST_FLAGS_AUTO_REWARDED))
                 RewardQuest(qInfo, 0, this, false);
-            else
-                SendQuestComplete(quest_id);
-        }
     }
 }
 
