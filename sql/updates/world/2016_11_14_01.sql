02be7b3ec21d79bdab78e055c9d3904df7d49ef0
zamalaev <zgn88@mail.ru>
2016-11-14 12:37:20 +0300

    Core/DB: fix vanilla boss immunities

---
+++ sql/updates/world/2016_11_14_01.sql
@@ -0,0 +1,40 @@
+-- SHADOWFANG KEEP TBC
+UPDATE creature_template SET mechanic_immune_mask=mechanic_immune_mask|76234719 WHERE entry IN (4275,3887,4278,4274,4279,3886,3914,14682,3927);
+-- BLACKFATHOM DEEPS TBC
+UPDATE creature_template SET mechanic_immune_mask=mechanic_immune_mask|76234719 WHERE entry IN (4829,6243,4887,4831,12902,4830,4832);
+-- THE STOCKADES TBC
+UPDATE creature_template SET mechanic_immune_mask=mechanic_immune_mask|76234719 WHERE entry IN (1716,1663,1717,1666,1696);
+-- RAZORFEN KRAUL TBC
+UPDATE creature_template SET mechanic_immune_mask=mechanic_immune_mask|76234719 WHERE entry IN (4422,4424,4421,4428,4842,4420,6168);
+-- GNOMEREGON TBC
+UPDATE creature_template SET mechanic_immune_mask=mechanic_immune_mask|76234719 WHERE entry IN (6229,6235,7361,7800,6231,7079);
+-- RAZORFEN DOWNS TBC
+UPDATE creature_template SET mechanic_immune_mask=mechanic_immune_mask|76234719 WHERE entry IN (12865,7358,8567,7357,7356,7355);
+-- SCARLET MONASTRY ARMORY TBC
+UPDATE creature_template SET mechanic_immune_mask=mechanic_immune_mask|76234719 WHERE entry IN (3975);
+-- SCARLET MONASTRY CATHEDRAL TBC
+UPDATE creature_template SET mechanic_immune_mask=mechanic_immune_mask|76234719 WHERE entry IN (4542,3976,3977);
+-- SCARLET MONASTRY GRAVEYARD TBC
+UPDATE creature_template SET mechanic_immune_mask=mechanic_immune_mask|76234719 WHERE entry IN (3983,4543,14693);
+-- SCARLET MONASTRY LIBRARY TBC
+UPDATE creature_template SET mechanic_immune_mask=mechanic_immune_mask|76234719 WHERE entry IN (3974,6487);
+-- ULDAMAN TBC
+UPDATE creature_template SET mechanic_immune_mask=mechanic_immune_mask|76234719 WHERE entry IN (7206,2748,6906,6907,7291,4854,7228,6908,6910);
+-- ZUL'FARRAK TBC
+UPDATE creature_template SET mechanic_immune_mask=mechanic_immune_mask|76234719 WHERE entry IN (8127,7267,7273,7795,7796,7797,7275,7272,7271);
+-- THE SUNKEN TEMPLE TBC
+UPDATE creature_template SET mechanic_immune_mask=mechanic_immune_mask|76234719 WHERE entry IN (8580,8443,5721,5713,5722,5715,5710,5401,5714,5717,5719,5711,5709,5708,5399,5720,5400,5712,5716);
+-- Maraudon TBC
+UPDATE creature_template SET mechanic_immune_mask=mechanic_immune_mask|76234719 WHERE entry IN (12225,12203,12236,13718,13282,12201,12258,13596,13601);
+-- BLACKROCK DEPTHS TBC
+UPDATE creature_template SET mechanic_immune_mask=mechanic_immune_mask|76234719 WHERE entry IN (9025,9016,9319,9018,10096,9024,9033,8983,9543,9537,9499,9502,9017,9056,9041,9042,9156,9938,8929,9019);
+-- DIRE MAUL TBC
+UPDATE creature_template SET mechanic_immune_mask=mechanic_immune_mask|76234719 WHERE entry IN (11490,13280,14327,11492,14326,14322,14321,14323,14325,11501,14324,11489,11488,11487,11496,11486);
+-- STRATHOLME TBC
+UPDATE creature_template SET mechanic_immune_mask=mechanic_immune_mask|76234719 WHERE entry IN (10516,10558,10808,11032,10997,10811,10813,10436,10437,10438,10435,10439,10440);
+-- SCHOLOMANCE TBC
+UPDATE creature_template SET mechanic_immune_mask=mechanic_immune_mask|76234719 WHERE entry IN (14861,10506,14695,10503,11622,14516,10433,10432,10508,16118,11261,10901,10505,10502,10504,10507,1853);
+-- LOWER BLACKROCK SPIRE TBC
+UPDATE creature_template SET mechanic_immune_mask=mechanic_immune_mask|76234719 WHERE entry IN (10263,9219,9218,9217,9196,9236,9237,16080,9596,10596,10376,9736,10220,10268,9718,9568);
+-- UPPER BLACKROCK SPIRE TBC
+UPDATE creature_template SET mechanic_immune_mask=mechanic_immune_mask|76234719 WHERE entry IN (9816,10264,10509,10899,10339,10429,10430,16042,10363);
