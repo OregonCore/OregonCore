c141adbee19b883eeaec6486c5f8940bde446f8b
zamalaev <zgn88@mail.ru>
2016-12-10 08:08:34 +0300

    Core/DB: rename and fixex sql

--- sql/updates/world/2016_12_09_03.sql
+++ sql/updates/world/2016_12_09_03.sql
@@ -1,3 +1,5 @@
+DELETE FROM `script_texts`  WHERE `entry` IN (-1189001, -1189002, -1189003, -1189004, -1189005, -1189006, -1189007);
+DELETE FROM `script_texts` WHERE `entry` IN (-1123682, -1123683, -1123684, -1123685, -1123686, -1123687, -1123688);
 INSERT INTO `script_texts` (`entry`, `content_default`, `content_loc1`, `content_loc2`, `content_loc3`, `content_loc4`, `content_loc5`, `content_loc6`, `content_loc7`, `content_loc8`, `sound`, `type`, `language`, `emote`, `comment`) VALUES 
 (-1123682, 'It is over, your search is done. Let fate choose now, the righteous one.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 11961, 1, 0, 0, 'HeadlessHorsman SAY_AGGRO'),
 (-1123683, 'Your body lies beaten, battered and broken. Let my curse be your own, fate has spoken.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 11962, 1, 0, 0, 'Headless Horseman PLAYER_DEATH'),
