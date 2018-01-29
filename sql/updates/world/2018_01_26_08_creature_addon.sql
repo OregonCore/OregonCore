66c914f3f5724cc71f53a0513b9e9fd8cbd371f5
AbraKabastard <80sVillain@gmail.com>
2018-01-29 09:15:58 -0400

    Attempt to resolve travis issues

--- sql/updates/world/2018_01_26_08_creature_addon.sql
+++ sql/updates/world/2018_01_26_08_creature_addon.sql
@@ -1,7 +1,4 @@
-ALTER TABLE `creature_addon`
- DROP COLUMN `moveflags`,
- DROP COLUMN `isActive`;
- 
-ALTER TABLE `creature_template_addon`
- DROP COLUMN `moveflags`,
- DROP COLUMN `isActive`;
+ ALTER TABLE `creature_addon` DROP COLUMN `moveflags`;
+ ALTER TABLE `creature_addon` DROP COLUMN `isActive`;
+ ALTER TABLE `creature_template_addon` DROP COLUMN `moveflags`;
+ ALTER TABLE `creature_template_addon` DROP COLUMN `isActive`;
