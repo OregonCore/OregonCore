b33adf27e019786fdcf0be9fcbccbe97b827a0c0
AbraKabastard <80sVillain@gmail.com>
2018-01-30 22:41:17 -0400

    SQL: Fix sql error

--- sql/updates/world/2018_01_28_03_creature_modifiers.sql
+++ sql/updates/world/2018_01_28_03_creature_modifiers.sql
@@ -8,7 +8,7 @@ ALTER TABLE creature_template ADD COLUMN `ArmorModifier` FLOAT NOT NULL DEFAULT 
 -- Formel to get Health Mod
 UPDATE creature_template ct 
 JOIN creature_classlevelstats cls ON ct.minlevel = cls.level AND ct.unit_class = cls.class
-SET ct.HealthModifier =  (ct.minhealth / cls.basehp0) WHERE ct.exp = "0";
+SET ct.HealthModifier = (ct.minhealth / cls.basehp0) WHERE ct.exp = "0";
 
 UPDATE creature_template ct 
 JOIN creature_classlevelstats cls ON ct.minlevel = cls.level AND ct.unit_class = cls.class
@@ -17,7 +17,7 @@ SET ct.HealthModifier = (ct.minhealth / cls.basehp1) WHERE ct.exp = "1";
 -- Formel to get Armor Mod
 UPDATE creature_template ct 
 JOIN creature_classlevelstats cls ON ct.minlevel = cls.level AND ct.unit_class = cls.class
-SET ct.ArmorModifier =  (ct.armor / cls.basearmor) WHERE ct.exp = "0";
+SET ct.ArmorModifier = (ct.armor / cls.basearmor) WHERE ct.exp = "0";
 
 UPDATE creature_template ct 
 JOIN creature_classlevelstats cls ON ct.minlevel = cls.level AND ct.unit_class = cls.class
@@ -26,8 +26,8 @@ SET ct.ArmorModifier = (ct.armor / cls.basearmor) WHERE ct.exp = "1";
 -- Formel to get Mana Mod
 UPDATE creature_template ct 
 JOIN creature_classlevelstats cls ON ct.minlevel = cls.level AND ct.unit_class = cls.class
-SET ct.ManaModifier =  (ct.minmana / cls.basemana) WHERE ct.exp = "0";
+SET ct.ManaModifier = (IFNULL(ct.minmana / cls.basemana, 1)) WHERE ct.exp = "0";
 
 UPDATE creature_template ct 
 JOIN creature_classlevelstats cls ON ct.minlevel = cls.level AND ct.unit_class = cls.class
-SET ct.ManaModifier = (ct.minmana / cls.basemana) WHERE ct.exp = "1";
+SET ct.ManaModifier = (IFNULL(ct.minmana / cls.basemana, 1)) WHERE ct.exp = "1";
