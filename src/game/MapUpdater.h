db10b745e477e9eb618e8ccb6c6a937fe270033c
AbraKabastard <80sVillain@gmail.com>
2018-02-01 23:22:35 -0400

    Core/Build: Fix build on nix

--- src/game/MapUpdater.h
+++ src/game/MapUpdater.h
@@ -29,7 +29,7 @@ class MapUpdater
 {
     public:
 
-        MapUpdater::MapUpdater() : m_executor(), m_mutex(), m_condition(m_mutex), pending_requests(0) {}
+        MapUpdater() : m_executor(), m_mutex(), m_condition(m_mutex), pending_requests(0) {}
         ~MapUpdater() { };
 
         friend class MapUpdateRequest;
