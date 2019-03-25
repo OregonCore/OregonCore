13443c1285cf6a497ba0d97b606aadbbd4d47eca
Lee <Talamortis@gmail.com>
2019-03-25 23:29:56 +0000

    [Boss / Script] gatewatcher Ironhand
    Reworked boss for gatewatcher Ironhand

--- src/scripts/Outland/TempestKeep/the_mechanar/boss_gatewatcher_ironhand.cpp
+++ src/scripts/Outland/TempestKeep/the_mechanar/boss_gatewatcher_ironhand.cpp
@@ -1,146 +1,87 @@
-/*
- * This file is part of the OregonCore Project. See AUTHORS file for Copyright information
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation; either version 2 of the License, or (at your
- * option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
- * more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program. If not, see <http://www.gnu.org/licenses/>.
- */
-
-/* ScriptData
-SDName: Boss_Gatewatcher_Ironhand
-SD%Complete: 75
-SDComment:
-SDCategory: Tempest Keep, The Mechanar
-EndScriptData */
-
 #include "ScriptMgr.h"
 #include "ScriptedCreature.h"
 
-#define SAY_AGGRO_1                     -1554006
-#define SAY_HAMMER_1                    -1554007
-#define SAY_HAMMER_2                    -1554008
-#define SAY_SLAY_1                      -1554009
-#define SAY_SLAY_2                      -1554010
-#define SAY_DEATH_1                     -1554011
-#define EMOTE_HAMMER                    -1554012
+enum GateWatcherIronHand
+{
+    SAY_AGGRO_1 = -1554006,
+    SAY_HAMMER_1 = -1554007,
+    SAY_HAMMER_2 = -1554008,
+    SAY_SLAY_1 = -1554009,
+    SAY_SLAY_2 = -1554010,
+    SAY_DEATH_1 = -1554011,
+    EMOTE_HAMMER = -1554012,
 
-// Spells to be casted
-#define SPELL_SHADOW_POWER              35322
-#define H_SPELL_SHADOW_POWER            39193
-#define SPELL_HAMMER_PUNCH              35326
-#define SPELL_JACKHAMMER                35327
-#define H_SPELL_JACKHAMMER              39194
-#define SPELL_STREAM_OF_MACHINE_FLUID   35311
+    SPELL_SHADOW_POWER = 35322,
+    H_SPELL_SHADOW_POWER = 39193,
+    SPELL_HAMMER_PUNCH = 35326,
+    SPELL_JACKHAMMER = 35327,
+    H_SPELL_JACKHAMMER = 39194,
+    SPELL_STREAM_OF_MACHINE_FLUID = 35311,
 
-// Gatewatcher Iron-Hand AI
+    EVENT_SHADOW_POWER = 1,
+    EVENT_STREAM_OF_MACHINE_FLUID = 2,
+    EVENT_JACKHAMMER = 3
+};
+
 struct boss_gatewatcher_iron_handAI : public ScriptedAI
 {
     boss_gatewatcher_iron_handAI(Creature* c) : ScriptedAI(c)
     {
         pInstance = (ScriptedInstance*)c->GetInstanceData();
-        HeroicMode = me->GetMap()->IsHeroic();
     }
 
     ScriptedInstance* pInstance;
-
-    bool HeroicMode;
-
-    uint32 Shadow_Power_Timer;
-    uint32 Jackhammer_Timer;
-    uint32 Stream_of_Machine_Fluid_Timer;
+    EventMap events;
 
     void Reset()
     {
-        Shadow_Power_Timer = 25000;
-        Jackhammer_Timer = 45000;
-        Stream_of_Machine_Fluid_Timer = 55000;
-
+        events.Reset();
     }
     void EnterCombat(Unit* /*who*/)
     {
         DoScriptText(SAY_AGGRO_1, me);
+        events.ScheduleEvent(EVENT_STREAM_OF_MACHINE_FLUID, 15000);
+        events.ScheduleEvent(EVENT_JACKHAMMER, 35000);
+        events.ScheduleEvent(EVENT_SHADOW_POWER, 25000);
     }
 
-    void KilledUnit(Unit* /*victim*/)
+    void KilledUnit(Unit* victim)
     {
-        if (rand() % 2)
-            return;
-
-        switch (rand() % 2)
-        {
-        case 0:
-            DoScriptText(SAY_SLAY_1, me);
-            break;
-        case 1:
-            DoScriptText(SAY_SLAY_2, me);
-            break;
-        }
+        if (victim->GetTypeId() == TYPEID_PLAYER)
+            DoScriptText(RAND(SAY_SLAY_1, SAY_SLAY_2), me);
     }
 
     void JustDied(Unit* /*Killer*/)
     {
         DoScriptText(SAY_DEATH_1, me);
-
-        if (!pInstance)
-            return;
-
-        //@todo Add door check/open code
     }
 
     void UpdateAI(const uint32 diff)
     {
-        //Return since we have no target
         if (!UpdateVictim())
             return;
 
-        //Shadow Power
-        if (Shadow_Power_Timer <= diff)
-        {
-            DoCast(me, HeroicMode ? H_SPELL_SHADOW_POWER : SPELL_SHADOW_POWER);
-            Shadow_Power_Timer = 20000 + rand() % 8000;
-        }
-        else Shadow_Power_Timer -= diff;
+        events.Update(diff);
+        if (me->HasUnitState(UNIT_STATE_CASTING))
+            return;
 
-        //Jack Hammer
-        if (Jackhammer_Timer <= diff)
+        switch (events.ExecuteEvent())
         {
-            //@todo expect cast this about 5 times in a row (?), announce it by emote only once
+        case EVENT_STREAM_OF_MACHINE_FLUID:
+            me->CastSpell(me->GetVictim(), SPELL_STREAM_OF_MACHINE_FLUID, false);
+            events.ScheduleEvent(EVENT_STREAM_OF_MACHINE_FLUID, 20000);
+            break;
+        case EVENT_JACKHAMMER:
             DoScriptText(EMOTE_HAMMER, me);
-            DoCastVictim(HeroicMode ? H_SPELL_JACKHAMMER : SPELL_JACKHAMMER);
-
-            //chance to yell, but not same time as emote (after spell in fact casted)
-            if (rand() % 2)
-                return;
-
-            switch (rand() % 2)
-            {
-            case 0:
-                DoScriptText(SAY_HAMMER_1, me);
-                break;
-            case 1:
-                DoScriptText(SAY_HAMMER_2, me);
-                break;
-            }
-            Jackhammer_Timer = 30000;
+            DoScriptText(RAND(SAY_HAMMER_1, SAY_HAMMER_2), me);
+            me->CastSpell(me, SPELL_JACKHAMMER, false);
+            events.ScheduleEvent(EVENT_JACKHAMMER, 40000);
+            break;
+        case EVENT_SHADOW_POWER:
+            me->CastSpell(me, SPELL_SHADOW_POWER, false);
+            events.ScheduleEvent(EVENT_SHADOW_POWER, 25000);
+            break;
         }
-        else Jackhammer_Timer -= diff;
-
-        //Stream of Machine Fluid
-        if (Stream_of_Machine_Fluid_Timer <= diff)
-        {
-            DoCastVictim(SPELL_STREAM_OF_MACHINE_FLUID);
-            Stream_of_Machine_Fluid_Timer = 35000 + rand() % 15000;
-        }
-        else Stream_of_Machine_Fluid_Timer -= diff;
 
         DoMeleeAttackIfReady();
     }
