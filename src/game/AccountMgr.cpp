fcb19e02a924790421ddcb44b3076f8edec5ad5a
AbraKabastard <80sVillain@gmail.com>
2019-05-08 10:35:20 -0300

    [Core/Console] Default account expansion set to The Burning Crusade (1)
    Only affects accounts created through the console
    
    Co-Authored-By: Lee <talamortis@users.noreply.github.com>

--- src/game/AccountMgr.cpp
+++ src/game/AccountMgr.cpp
@@ -46,8 +46,9 @@ AccountOpResult AccountMgr::CreateAccount(std::string username, std::string pass
     if (result)
         return AOR_NAME_ALREDY_EXIST;                       // username does already exist
 
-    if (!LoginDatabase.PExecute("INSERT INTO account(username,sha_pass_hash,joindate) VALUES('%s',SHA1(CONCAT('%s',':','%s')),NOW())", username.c_str(), username.c_str(), password.c_str()))
+    if (!LoginDatabase.PExecute("INSERT INTO account(username,sha_pass_hash,joindate, expansion) VALUES('%s',SHA1(CONCAT('%s',':','%s')),NOW(), 1)", username.c_str(), username.c_str(), password.c_str()))
         return AOR_DB_INTERNAL_ERROR;                       // unexpected error
+    
     LoginDatabase.Execute("INSERT INTO realmcharacters (realmid, acctid, numchars) SELECT realmlist.id, account.id, 0 FROM realmlist,account LEFT JOIN realmcharacters ON acctid=account.id WHERE acctid IS NULL");
 
     return AOR_OK;                                          // everything's fine
