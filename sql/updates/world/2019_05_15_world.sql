ae28fb0d473305952d15defb324421d46404f3e7
AbraKabastard <80svillain@gmail.com>
2019-05-15 13:35:33 -0300

    DB/GameObjects: Fix data for brazier in shattrath
    closes #1630

---
+++ sql/updates/world/2019_05_15_world.sql
@@ -0,0 +1,2 @@
+DELETE FROM `gameobject_template` WHERE `entry`=185968;
+INSERT INTO `gameobject_template` (`entry`, `type`, `displayId`, `name`, `castBarCaption`, `faction`, `flags`, `size`, `data0`, `data1`, `data2`, `data3`, `data4`, `data5`, `data6`, `data7`, `data8`, `data9`, `data10`, `data11`, `data12`, `data13`, `data14`, `data15`, `data16`, `data17`, `data18`, `data19`, `data20`, `data21`, `data22`, `data23`, `AIName`, `ScriptName`) VALUES (185968, 8, 7403, 'Brazier', '', 0, 0, 1, 4, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', '');
