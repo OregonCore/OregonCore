9aef853ff90958b3679c94d3c065726b641104e5
AbraKabastard <80svillain@gmail.com>
2019-05-20 22:10:27 -0300

    DB/Spells: Remove instant cast spell for Magma Totem.
    Closes #1440

---
+++ sql/updates/world/2019_05_20_world.sql
@@ -0,0 +1,6 @@
+-- Magma Totem
+UPDATE creature_template SET spell2 = 0 WHERE entry = 5929;
+UPDATE creature_template SET spell2 = 0 WHERE entry = 7464;
+UPDATE creature_template SET spell2 = 0 WHERE entry = 7465;
+UPDATE creature_template SET spell2 = 0 WHERE entry = 7466;
+UPDATE creature_template SET spell2 = 0 WHERE entry = 15484;
